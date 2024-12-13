9ea2a731835adf04d9bf47be82cadb5f09454ce3
gudo1234 <izumilitee@gmail.com>
2024-12-13 17:43:52 -0600

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,6 @@ import cheerio from 'cheerio'
 import fetch from 'node-fetch'
 import axios from 'axios' 
 import moment from 'moment-timezone'
-import { en, es, id, ar, pt } from './lib/idiomas/total-idiomas.js'
 
 //---------[ Añada los numeros a ser Propietario/a ]---------
 

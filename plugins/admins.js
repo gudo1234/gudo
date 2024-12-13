18293ed5126972e3b786872b4a23bc61686ec543
gudo1234 <izumilitee@gmail.com>
2024-12-13 16:23:05 -0600

    Update admins.js

--- plugins/admins.js
+++ plugins/admins.js
@@ -5,7 +5,7 @@ const handler = async (m, {conn, participants, groupMetadata, args}) => {
   const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
   const pesan = args.join` `;
   const oi = `Mensaje: ${pesan}`;
-  const text = `> invocando Admins del Grupo.
+  const text = `> invocando Admins del Grupo...
 ${oi}\n━━━✦✗✦━━━
 ${listAdmin}\n━━━✦✗✦━━━
 > Use este comando cuando se trate de una emergencia. ¡Sea activo y pronto estará en esta lista!`.trim();

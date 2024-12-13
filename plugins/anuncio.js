b3ae99b750470e01d1e818d736a1f7f12822451c
gudo1234 <izumilitee@gmail.com>
2024-12-13 13:57:30 -0600

    Update anuncio.js

--- plugins/anuncio.js
+++ plugins/anuncio.js
@@ -1,9 +1,10 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 conn.reply(m.chat, 'Hola', m, {
+let img = await (await fetch(`https://qu.ax/casQP.jpg`)).buffer()
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: wm,
 body: 'test',
-previewType: 0, thumbnail: imagen4,
+previewType: 0, thumbnail: img,
 sourceUrl: canal }}});
 }
 

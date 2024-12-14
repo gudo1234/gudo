fd986a25db27e65c128fca906be696c1bf235a1c
gudo1234 <izumilitee@gmail.com>
2024-12-14 16:39:59 -0600

    Update welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -3,8 +3,7 @@ import { sticker } from '../lib/sticker.js'
 import fetch from 'node-fetch';
 export async function before(m, { conn, participants, groupMetadata }) {
 
-/*let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }*/
-  if (!m.messageStubType || !m.isGroup) return true;
+if (!m.messageStubType || !m.isGroup) return true;
 let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://qu.ax/casQP.jpg')
   let img = await (await fetch(`${pp}`)).buffer()
   let vn2 = './media/adios.mp3';
@@ -24,6 +23,18 @@ if (media === 'stiker')
 conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `👋🏻ADIOS +${m.messageStubParameters[0].split`@`[0]}`, body: 'Esperemos que no vuelva -_-', mediaType: 2, sourceUrl: 'https://whatsapp.com/channel/0029VaXHNMZL7UVTeseuqw3H', thumbnail: img}}}, { quoted: m })
 
 if (media === 'audio')
-await this.sendMessage(m.chat, { audio: { url: vn2 }, mimetype: 'audio/mpeg', ptt: true, contextInfo: { externalAdReply: { title: `👋🏻 ADIOS +${m.messageStubParameters[0].split`@`[0]}`, body: 'Esperemos que no vuelva -_-', thumbnailUrl: null, thumbnail: img, showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
+this.sendMessage(m.chat, { audio: { url: vn2 }, 
+    contextInfo: { forwardedNewsletterMessageInfo: { 
+    newsletterJid: id_canal, 
+    serverMessageId: '', 
+    newsletterName: wm }, forwardingScore: 9999999, isForwarded: true, mentionedJid: getMentionedJid(), "externalAdReply": { 
+    "title": `👋🏻 ADIOS +${m.messageStubParameters[0].split`@`[0]}`, 
+    "body": 'Esperemos que no vuelva -_-', 
+    "previewType": "PHOTO", 
+    "thumbnailUrl": null,
+    "thumbnail": img, 
+    "sourceUrl": 'https://www.atom.bio/edar_', 
+    "showAdAttribution": true}}, 
+     seconds: '4556', ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
   }
   }

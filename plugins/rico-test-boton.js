ae2af4fb85359c8dd8ff5bc491c8b13ceca57e84
gudo1234 <izumilitee@gmail.com>
2024-12-14 16:04:40 -0600

    Delete plugins directory

--- plugins/rico-test-boton.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-
-  conn.sendButton(m.chat, ' 💀' +'bug', `test`, null, [['run', `run`], ['run', `run`], ['run', `run`], ['run', `run`], ['run', `run`], ['run', `run`]], null, null, null)
-}
-
-handler.customPrefix = /^(test)$/i
-handler.command = new RegExp
-export default handler

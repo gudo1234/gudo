8086aace1a65dbfed718826a86cd8ba72eca4549
gudo1234 <izumilitee@gmail.com>
2024-12-14 14:47:52 -0600

    Update anuncio.js

--- plugins/anuncio.js
+++ plugins/anuncio.js
@@ -3,5 +3,4 @@ let name = await conn.getName(m.sender)
 await conn.sendMessage(m.chat, { text: global.espere + `*${name}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, humbnailUrl: imagen4, thumbnail: imagen4, showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
 }
 handler.command = /^(ve)$/i
-handler.group = true;
 export default handler

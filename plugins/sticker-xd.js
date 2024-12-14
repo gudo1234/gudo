aa3cce53e0baf1e1112a4a2ad13acad1f4812bbb
gudo1234 <izumilitee@gmail.com>
2024-12-14 09:36:44 -0600

    Update sticker-xd.js

--- plugins/sticker-xd.js
+++ plugins/sticker-xd.js
@@ -16,7 +16,6 @@ function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
 let stikerxd = [
-'https://qu.ax/EDQhG.jpg',
 'https://telegra.ph/file/913f5861cefbdde379921.jpg',
 'https://i.ibb.co/SK4Zqkt/file.webp',
 'https://telegra.ph/file/6b7b0dbf022ee46a44887.jpg',

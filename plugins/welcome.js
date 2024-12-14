05ef374e06a461f3afb86bde8aebf35f93da5eee
gudo1234 <izumilitee@gmail.com>
2024-12-14 17:25:54 -0600

    Update welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -3,7 +3,8 @@ import { sticker } from '../lib/sticker.js'
 import fetch from 'node-fetch';
 export async function before(m, { conn, participants, groupMetadata }) {
 
-if (!m.messageStubType || !m.isGroup) return true;
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+  if (!m.messageStubType || !m.isGroup) return true;
 let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://qu.ax/casQP.jpg')
   let img = await (await fetch(`${pp}`)).buffer()
   let vn2 = './media/adios.mp3';
@@ -37,4 +38,4 @@ this.sendMessage(m.chat, { audio: { url: vn2 },
     "showAdAttribution": true}}, 
      seconds: '4556', ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
   }
-  }
+}

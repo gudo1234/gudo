fe77d1e0b771af67d5049e6d6de284aefcdb2c75
gudo1234 <izumilitee@gmail.com>
2024-12-14 13:32:44 -0600

    Create anuncio.js

---
+++ plugins/anuncio.js
@@ -0,0 +1,6 @@
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+await conn.sendMessage(m.chat, { text: 'hola', contextInfo: { externalAdReply: {title: '꙳🧧𓆩ίʑ᭘ɱί-ⲃⲟτ𓆪🧧꙳', thumbnailUrl: imagen4, showAdAttribution: true, sourceUrl: canal}}} , { quoted: m })
+}
+handler.command = /^(ve)$/i
+handler.group = true;
+export default handler

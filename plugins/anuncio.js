9452c97d235984e4b1638454eb20698d7f2dea82
gudo1234 <izumilitee@gmail.com>
2024-12-14 14:36:48 -0600

    Update anuncio.js

--- plugins/anuncio.js
+++ plugins/anuncio.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-//await conn.sendMessage(m.chat, { text: nombre, contextInfo: { externalAdReply: {title: '꙳🧧𓆩ίʑ᭘ɱί-ⲃⲟτ𓆪🧧꙳', thumbnailUrl: imagen4, thumbnail: imagen4, showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
-global.www
+let name = await conn.getName(m.sender)
+await conn.sendMessage(m.chat, { text: `global.espere *${name}*`, contextInfo: { externalAdReply: {title: `${wm}`, thumbnailUrl: imagen4, thumbnail: imagen4, showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
 }
 handler.command = /^(ve)$/i
 handler.group = true;

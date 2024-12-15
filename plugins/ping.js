8a5a0067a6a9e8b6237a0266a2021ebdde19f16f
gudo1234 <izumilitee@gmail.com>
2024-12-15 16:44:04 -0600

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
          exec(`neofetch --stdout`, (error, stdout, stderr) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
-m.reply(`{[${latensi.toFixed(4)}]}, •ms...`);
+m.reply(`[  ${latensi.toFixed(4)}  ], ms...`);
             });
 }
 handler.command = /^(run|ping|speed|speedtest|velocidad)$/i;

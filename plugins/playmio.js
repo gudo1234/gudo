b3a763542852bf22026de03f93c8f3e0c5ca31cd
gudo1234 <izumilitee@gmail.com>
2024-12-17 10:31:46 -0600

    Update playmio.js

--- plugins/playmio.js
+++ plugins/playmio.js
@@ -226,7 +226,7 @@ console.log(e2);
 }}}
 
 }
-handler.help = ['play2', 'play3', 'play4', 'playdoc'];
+handler.help = ['play', 'play2', 'play3', 'play4', 'playdoc'];
 handler.tags = ['descargas'];
 handler.command = ['play', 'play2', 'play3', 'play4', 'mp3', 'mp4', 'playdoc', 'playdoc2']
 handler.group = true;

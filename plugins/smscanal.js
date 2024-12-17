5e4984e8964698b61e377fe77d039c04e5073fa1
gudo1234 <izumilitee@gmail.com>
2024-12-17 14:34:34 -0600

    Update smscanal.js

--- plugins/smscanal.js
+++ plugins/smscanal.js
@@ -2,13 +2,13 @@ let handler = async (m, { conn, usedPrefix, text }) => {
 if (!text) throw `🌱 Ingresa el texto que se enviará junto a los botones.`
 let vn = './media/ocupado'
 conn.sendButtonMessages('120363285614743024@newsletter', [
-[text, '𝗉𝗈𝗐𝖾𝗋𝖾𝖽 ⓒ 𝖨𝗓𝗎𝗆𝗂-𝖻𝗈𝗍 𝟤𝟢𝟤𝟢-𝟤𝟢𝟤𝟦.', 'https://qu.ax/QVbYR.jpg' , [
+[`🚩 ${text}`, '𝗉𝗈𝗐𝖾𝗋𝖾𝖽 ⓒ 𝖨𝗓𝗎𝗆𝗂-𝖻𝗈𝗍 𝟤𝟢𝟤𝟢-𝟤𝟢𝟤𝟦.', 'https://qu.ax/QVbYR.jpg' , [
 ], null, [
 ['Desarrollador', edar], ['Unirme al grupo', 'https://tinyurl.com/yc2fdh5d']
 ],
 []
 ]], m)
-conn.sendFile('120363285614743024@newsletter', vn, 'error.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+conn.sendFile('120363285614743024@newsletter', vn, 'error.mp3', null, null, true, { type: 'audioMessage', ptt: true })
 }
 handler.command = /^(best)$/i
 export default handler

aa88f51648a065dee69b56f98b1dc6b11a0a9fb1
gudo1234 <izumilitee@gmail.com>
2024-12-17 00:04:29 -0600

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -43,6 +43,8 @@ if (size.split('MB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas
        txt += `	✩   *Tamaño* : ${size}\n\n`
        txt += `> *- ↻ El video se esta enviando espera un momento, soy lenta. . .*`
 /**await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)**/
+let name = await conn.getName(m.sender)
+await conn.sendMessage(m.chat, { text: global.espere + `*${name}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, thumbnailUrl: imagen4, thumbnail: imagen4, showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
 await star.sendMessage(m.chat, { video: { url: dl_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
 await m.react('✅')
 } catch {
@@ -58,6 +60,8 @@ if (size.split('MB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas
        txt += `	✩   *Tamaño* : ${size}\n\n`
        txt += `> *- ↻ El video se esta enviando espera un momento, soy lenta. . .*`
 /**await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)**/
+let name = await conn.getName(m.sender)
+await conn.sendMessage(m.chat, { text: global.espere + `*${name}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, thumbnailUrl: imagen4, thumbnail: imagen4, showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
 await star.sendMessage(m.chat, { video: { url: dl_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
 await m.react('✅')
 } catch {

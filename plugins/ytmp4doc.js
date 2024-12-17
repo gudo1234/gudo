f80769b699781601b1d10dbf3151a21b15cd152c
gudo1234 <izumilitee@gmail.com>
2024-12-17 01:28:13 -0600

    Update ytmp4doc.js

--- plugins/ytmp4doc.js
+++ plugins/ytmp4doc.js
@@ -8,7 +8,8 @@ let handler = async (m, { conn: star, args, text, isPrems, isOwner, usedPrefix, 
 if (!args || !args[0]) return star.reply(m.chat, '🚩 Ingresa el enlace del vídeo de YouTube junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`, m, rcanal)
 if (!args[0].match(/youtu/gi)) return star.reply(m.chat, `Verifica que el enlace sea de YouTube.`, m, rcanal).then(_ => m.react('✖️'))
 let q = args[1] || '360p'
-
+let name = await conn.getName(m.sender)
+await conn.sendMessage(m.chat, { text: global.espere + `*${name}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, thumbnailUrl: imagen4, thumbnail: imagen4, showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
 await m.react('🕓')
 try {
 let v = args[0]
@@ -45,8 +46,6 @@ if (size.split('GB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas
        txt += `	✩   *Tamaño* : ${size}\n\n`
        txt += `> *- ↻ El video se esta enviando espera un momento, soy lenta. . .*`
 /**await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)**/
-let name = await conn.getName(m.sender)
-await conn.sendMessage(m.chat, { text: global.espere + `*${name}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, thumbnailUrl: imagen4, thumbnail: imagen4, showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
 await star.sendMessage(m.chat, { document: { url: dl_url }, caption: '', mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
 await m.react('✅')
 } catch {
@@ -63,8 +62,6 @@ if (size.split('GB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas
        txt += `	✩   *Tamaño* : ${size}\n\n`
        txt += `> *- ↻ El video se esta enviando espera un momento, soy lenta. . .*`
 /**await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)**/
-let name = await conn.getName(m.sender)
-await conn.sendMessage(m.chat, { text: global.espere + `*${name}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, thumbnailUrl: imagen4, thumbnail: imagen4, showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
 await star.sendMessage(m.chat, { document: { url: dl_url }, caption: '', mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
 await m.react('✅')
 } catch {

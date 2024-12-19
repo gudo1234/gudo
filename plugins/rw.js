493ecba59edcc0d4949cf8111824eda25912bc73
gudo1234 <izumilitee@gmail.com>
2024-12-18 21:41:02 -0600

    Update rw.js

--- plugins/rw.js
+++ plugins/rw.js
@@ -1,18 +1,14 @@
 import util from 'util'
 import path from 'path'
 import fetch from 'node-fetch'
-let handler = async (m, { conn, groupMetadata, usedPrefix, command, text }) => {
+let handler = async (m, { conn, usedPrefix, command, text }) => {
 let res = await fetch('https://api.waifu.pics/sfw/waifu')
-let user = a => '@' + a.split('@')[0]
-let ps = groupMetadata.participants.map(v => v.id)
+let name = await conn.getName(m.sender)
 if (!res.ok) throw await res.text()
 let json = await res.json()
-let a = ps.getRandom()
-let k = Math.floor(Math.random() * 70);
 if (!json.url) throw `xd`
-//conn.sendFile(m.chat, json.url, 'error.jpg', `*Nombre:* Desconocido\n*Precio:* Gratis\n*Reclamado por:* ${user(a)}`, m, {contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: wm, body: 'waifu', previewType: 0, thumbnail: imagen4, sourceUrl: canal}}})
 //conn.sendButton(m.chat, `🥺`, wm, json.url, [['siguiente', `/${command}`]], m)
-await conn.sendFile(m.chat, json.url, 'error.jpg', `*Nombre:* Desconocido\n*Precio:* Gratis\n*Reclamado por:* ${user(a)}`, null, null, rcanal);
+await conn.sendFile(m.chat, json.url, 'error.jpg', `*Nombre:* Desconocido\n*Precio:* Gratis\n*Reclamado por:* ${name}`, null, null, rcanal);
 }
 handler.help = ['waifu']
 handler.tags = ['anime']

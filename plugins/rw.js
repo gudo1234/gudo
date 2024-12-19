cbbcf45ed70c034af6a64d133877f27fde543dfe
gudo1234 <izumilitee@gmail.com>
2024-12-18 20:59:03 -0600

    Update rw.js

--- plugins/rw.js
+++ plugins/rw.js
@@ -1,10 +1,16 @@
+import util from 'util'
+import path from 'path'
 import fetch from 'node-fetch'
-let handler = async (m, { conn, usedPrefix, command }) => {
+let handler = async (m, { conn, groupMetadata, usedPrefix, command }) => {
 let res = await fetch('https://api.waifu.pics/sfw/waifu')
+let user = a => '@' + a.split('@')[0]
+let ps = groupMetadata.participants.map(v => v.id)
 if (!res.ok) throw await res.text()
 let json = await res.json()
-if (!json.url) throw `${lenguajeGB['smsAvisoFG']()}`
-conn.sendFile(m.chat, json.url, 'error.jpg', `*Nombre:* Desconocido\n*Precio:* Gratis\n*Reclamado por:* tí`, m, {contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: ag, body: 'waifu', previewType: 0, thumbnail: imagen4, sourceUrl: [md, yt, tiktok].getRandom()}}})
+let a = ps.getRandom()
+let k = Math.floor(Math.random() * 70);
+if (!json.url) throw `xd`
+conn.sendFile(m.chat, json.url, 'error.jpg', `*Nombre:* Desconocido\n*Precio:* Gratis\n*Reclamado por:* ${user(a)}`, m, {contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: wm, body: 'waifu', previewType: 0, thumbnail: imagen4, sourceUrl: canal}}})
 //conn.sendButton(m.chat, `🥺`, wm, json.url, [['siguiente', `/${command}`]], m)
 }
 handler.help = ['waifu']
@@ -12,3 +18,5 @@ handler.tags = ['anime']
 handler.command = /^(waifu|rw)$/i
 handler.group = true;
 export default handler
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}

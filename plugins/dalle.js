4b62777d704bfff6acfa36473e12ddd141b13eef
gudo1234 <izumilitee@gmail.com>
2024-12-20 17:04:39 -0600

    Update dalle.js

--- plugins/dalle.js
+++ plugins/dalle.js
@@ -3,10 +3,10 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
     if (!text) throw `*🧧 ingrese una petición para generar una imagen con dalle...*`;
     m.react('🕒')
-    await conn.sendMessage(m.chat, {text: '*🧧 Espere un momento...*'}, {quoted: m});
+    let name = await conn.getName(m.sender)
+await conn.sendMessage(m.chat, { text: global.espere + `*${name}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, thumbnailUrl: imagen4, thumbnail: imagen4, showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
     
     try {
-      // api venom xov ❤️
         const response = await fetch(`https://eliasar-yt-api.vercel.app/api/ai/text2img?prompt=${encodeURIComponent(text)}`);
         if (!response.ok) throw new Error('Network response was not ok');
         

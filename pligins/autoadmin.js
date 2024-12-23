2bc7391b5fa4c299daac92a2b4ec360ca0c0e4ec
gudo1234 <izumilitee@gmail.com>
2024-12-22 20:35:59 -0600

    Delete pligins directory

--- pligins/autoadmin.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn, isAdmin }) => {
-if (m.fromMe) throw 'Nggk'
-if (isAdmin) throw 'Ya eres admin del grupo mi creador'
-await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")}
-handler.command = ['autoadmin']
-handler.rowner = true
-handler.botAdmin = true
-export default handler

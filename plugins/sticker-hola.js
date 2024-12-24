ce81032a468a2e2aba14e9222846df581e0a1de6
gudo1234 <izumilitee@gmail.com>
2024-12-24 06:04:22 -0600

    Update sticker-hola.js

--- plugins/sticker-hola.js
+++ plugins/sticker-hola.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://qu.ax/casQP.jpg')
   let im = await (await fetch(`${pp}`)).buffer()
 let stiker = await sticker(null, global.API(`${pickRandom(stikerxd)}`), global.packname, global.author)
-    if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `Holaaa`, body: '🌊', mediaType: 2, sourceUrl: canal, thumbnail: img}}}, { quoted: m })
+    if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `Holaaa`, body: '🌊', mediaType: 2, sourceUrl: canal, thumbnail: im}}}, { quoted: m })
     throw stiker.toString()
     
 }

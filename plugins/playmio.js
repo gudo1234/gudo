8d268950d7e18fc601cb502911000085b6637b86
gudo1234 <izumilitee@gmail.com>
2024-12-27 17:26:07 -0600

    Update playmio.js

--- plugins/playmio.js
+++ plugins/playmio.js
@@ -1,4 +1,4 @@
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+/*import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
 import yts from 'yt-search'
 import ytdl from 'ytdl-core'
@@ -322,5 +322,54 @@ if (data.status === 'ok') {
   } else {
     throw new Error("No se pudo obtener la descarga desde 9Convert");
   }
-  }
-  
+  }*/
+/* 
+
+[ Canal Principal ] :
+https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
+
+[ Canal Rikka Takanashi Bot ] :
+https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
+
+[ Canal StarlightsTeam] :
+https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
+
+[ HasumiBot FreeCodes ] :
+https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
+*/
+
+// *[ ❀ PLAY ]*
+import fetch from 'node-fetch'
+import yts from 'yt-search'
+
+let handler = async (m, { conn, text, args }) => {
+if (!text) {
+return m.reply("🧧 *ingresa un texto de lo que quieres buscar*")
+}
+    
+let ytres = await search(args.join(" "))
+let txt = `*Título* : ${ytres[0].title}
+*Duración* : ${ytres[0].timestap}
+*Publicado* : ${ytres[0].ago}
+*Canal* : ${ytres[0].author.name || 'Desconocido'}
+*Url* : ${'https://youtu.be/' + ytres[0].videoId}`
+await m.react('🕒')
+await conn.sendFile(m.chat, ytres[0].image, 'thumbnail.jpg', txt, m, null, rcanal)
+    
+try {
+let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp3?apikey=gifted&url=${ytres[0].url}`)
+let json = await api.json()
+let { quality, title, download_url } = json.result
+await conn.sendMessage(m.chat, { audio: { url: download_url }, fileName: `${title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })
+} catch (error) {
+console.error(error)
+}}
+
+handler.command = /^(play)$/i
+
+export default handler
+
+async function search(query, options = {}) {
+  let search = await yts.search({ query, hl: "es", gl: "ES", ...options })
+  return search.videos
+}

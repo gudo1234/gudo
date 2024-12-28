4fc9bdb39c570d9503b570b41b3332273d80bdd3
gudo1234 <izumilitee@gmail.com>
2024-12-28 10:58:41 -0600

    Update edi.js

--- plugins/edi.js
+++ plugins/edi.js
@@ -1,22 +1,31 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 let txt = `test`
-conn.sendMessage(m.chat, { text: txt, caption: "1234", footer: wm, buttons: [
-  {
-    buttonId: ".gay", 
-    buttonText: { 
-      displayText: 'yes' 
-    }
-  }, {
-    buttonId: ".gay", 
-    buttonText: {
-      displayText: "sii"
-    }
-  }
-],
-  viewOnce: true,
-  headerType: 1,
-  jpegthumbnail: imagen4,
-}, { quoted: m})
+m.reply({
+           image: {
+               url: "https://files.catbox.moe/yupd7z.jpg"
+           },
+           caption: 'hola',
+           mentions: [m.sender],
+           footer: wm,
+           buttons: [{
+             buttonId: ".gay",
+               buttonText: {
+                   displayText: "yes"
+              }
+          },{
+             buttonId: ".gay",
+               buttonText: {
+                   displayText: "si"
+              }
+          },{
+             buttonId: ".gay",
+               buttonText: {
+                   displayText: "yesi"
+              }
+          }],
+          viewOnce: true,
+          headerType: 6,
+       })
 }
 handler.command = ['edi']
 export default handler

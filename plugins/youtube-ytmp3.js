06b0f40eb2c7bda231223c4e4dec4f5efeefea7c
gudo1234 <izumilitee@gmail.com>
2024-12-28 08:53:34 -0600

    Update youtube-ytmp3.js

--- plugins/youtube-ytmp3.js
+++ plugins/youtube-ytmp3.js
@@ -13,26 +13,28 @@ https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
 https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
 */
 
-// *[ ❀ YTMP3 ]*
+// *[ ❀ YTMP3V2 ]*
 import fetch from 'node-fetch'
 
-let HS = async (m, { conn, text }) => {
+let HS = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) {
-return m.reply("🧧 *Ingresa un link de youtube*")
+return conn.reply(m.chat, `❀Ingresa el link de youtube `, m)
 }
 let name = await conn.getName(m.sender)
-await conn.sendMessage(m.chat, { text: global.espere + `*${name}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, thumbnailUrl: imagen4, thumbnail: imagen4, showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
+await conn.sendMessage(m.chat, { text: global.espere + `*${name}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, thumbnailUrl: imagen4, thumbnail: imagen4, showAdAttribution: true, sourceUrl: canal}}} , { quoted: m})
 try {
-let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp3?apikey=gifted&url=${text}`)
+let calidad = '128' // Calidades disponibles : 32, 64, 128, 192, 320
+let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp3q?apikey=gifted&quality=${calidad}&url=${text}`)
 let json = await api.json()
-let { quality, title, download_url } = json.result
-await conn.sendMessage(m.chat, { audio: { url: download_url }, fileName: `${title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })
+let { quality, title, download_url, thumbnail } = json.result
+
+
+await conn.sendMessage(m.chat, { audio: { url: download_url }, caption: null, mimetype: "audio/mpeg" }, { quoted: m })
 } catch (error) {
 console.error(error)
 m.reply(`error temporal, prueba con ${usedPrefix + command}2`)
 }}
 
-HS.command = ['ytmp3', 'mp3', 'yta'];
-HS.group = true;
+HS.command = ['ytmp3', 'mp3', 'yta']
 
 export default HS

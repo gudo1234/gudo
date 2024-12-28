c09e88018503278a425d4d63ba0b31f3c393e8da
gudo1234 <izumilitee@gmail.com>
2024-12-27 23:34:06 -0600

    Update edi.js

--- plugins/edi.js
+++ plugins/edi.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-//const {imageMessage} = await prepareWAMessageMedia({ image: { url: "https://i.pinimg.com/736x/1c/b9/dc/1cb9dce731c1544b0bd018b02567fd1f.jpg" }}, { upload: conn.waUploadToServer})
+let img = await prepareWAMessageMedia({ image: { url: "https://i.pinimg.com/736x/1c/b9/dc/1cb9dce731c1544b0bd018b02567fd1f.jpg" }}, { upload: conn.waUploadToServer})
   let txt = `test`
 conn.sendMessage(m.chat, { text: txt, caption: "1234", footer: wm, buttons: [
   {
@@ -16,7 +16,7 @@ conn.sendMessage(m.chat, { text: txt, caption: "1234", footer: wm, buttons: [
 ],
   viewOnce: true,
   headerType: 1,
-  imageMessage: imagen4
+  imageMessage: img
 }, { quoted: m})
 }
 handler.command = ['edi']

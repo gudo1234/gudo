b0ece2d3c7dddb074b20d57cff7a33c47f20c91b
gudo1234 <izumilitee@gmail.com>
2024-12-28 00:06:05 -0600

    Update edi.js

--- plugins/edi.js
+++ plugins/edi.js
@@ -15,7 +15,7 @@ conn.sendMessage(m.chat,  {image: {url: imagen4}}, { text: txt, caption: "1234",
 ],
   viewOnce: true,
   headerType: 1,
-  imageMessage: imagen4,
+  Thumbnail: imagen4,
 }, { quoted: m})
 }
 handler.command = ['edi']

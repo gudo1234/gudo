8bd6736e792a555a6dcbb3a35a80c0b7a69a9869
gudo1234 <izumilitee@gmail.com>
2024-12-29 02:47:35 -0600

    Update welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -18,7 +18,7 @@ let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch
   let userName = user ? user.name : await conn.getName(who);
   let or = ['stiker', 'audio', 'boton'];
   let media = or[Math.floor(Math.random() * 3)]
-  let stiker = await sticker(imagen7, false, global.packname, global.author)*/
+  let stiker = await sticker(imagen7, false, global.packname, global.author)
   if (chat.welcome && (m.messageStubType === 28 || m.messageStubType === 32)) {
 if (media === 'stiker')
 this.sendFile(m.chat, stiker, 'sticker.webp', '',null, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `👋🏻ADIOS +${m.messageStubParameters[0].split`@`[0]}`, body: 'Esperemos que no vuelva -_-', mediaType: 2, sourceUrl: 'https://whatsapp.com/channel/0029VaXHNMZL7UVTeseuqw3H', thumbnail: im}}}, { quoted: null })

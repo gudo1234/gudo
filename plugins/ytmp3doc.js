5374b3f328111889cde20997401fbbcc48c1671a
gudo1234 <izumilitee@gmail.com>
2024-12-29 00:32:06 -0600

    Update ytmp3doc.js

--- plugins/ytmp3doc.js
+++ plugins/ytmp3doc.js
@@ -14,8 +14,7 @@ await conn.sendMessage(m.chat, { text: global.espere + `*${name}*`, contextInfo:
         {
           const {data}= await axios.get(`https://deliriussapi-oficial.vercel.app/download/ytmp4?url=${enlace}`);
           const yt=data.data.download.url
-          //await conn.sendFile(m.chat,yt,'yt.mp4', wm, m, null)
-        await conn.sendMessage(m.chat, { document: { url: downloadUrl }, mimetype: 'audio/mpeg', fileName: `${name}.mp3` }, { quoted: m });
+          conn.sendMessage(m.chat, {document: {url:yt}, caption: wm, mimetype: 'audio/mpeg', fileName: `${name}` + `.mp4`}, {quoted: m})
         await m.react('✅');  
         }
         else m.reply("Ingresa el enlace del vídeo de YouTube")

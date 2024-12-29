c0703ab616e626bf71cd78740474c11c14849b7f
gudo1234 <izumilitee@gmail.com>
2024-12-29 17:02:42 -0600

    Update edi.js

--- plugins/edi.js
+++ plugins/edi.js
@@ -1,28 +1,32 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-let txt = `test`
-conn.sendMessage(m.chat, {
-           image: {
-               url: "https://files.catbox.moe/beawnt.jpg"
-           },
-           caption: 'holaa',
-           mentions: [m.sender],
-           footer: wm,
-           buttons: [{
-             buttonId: ".gay",
-               buttonText: {
-                   displayText: "yes"
-              }
-          },{
-             buttonId: ".gay",
-               buttonText: {
-                   displayText: "si"
-              }
-          }],
-          viewOnce: true,
-          headerType: 6,
-         mentions: [m.sender], }, 
-        { quoted: m});
-       })
-}
+const handler = async (m, { conn }) => {
+  const taguser = '@' + m.sender.split('@')[0]; // Obtiene el usuario etiquetado
+  
+  conn.sendMessage(m.chat, {
+    image: { url: 'https://files.catbox.moe/beawnt.jpg' },
+    caption: `Hola bebe xD`, // Mención visible del usuario
+    footer: wm,
+    buttons: [
+      {
+        buttonId: ".gay",
+        buttonText: {
+          displayText: "Yes",
+        },
+        type: 1,
+      },
+      {
+        buttonId: ".gay",
+        buttonText: {
+          displayText: "yes",
+        },
+        type: 1,
+      },
+    ],
+    viewOnce: true,
+    headerType: 4,
+    mentions: [m.sender], // Mención funcional
+  }, { quoted: m });
+};
+
 handler.command = ['edi']
-export default handler
+
+export default handler;

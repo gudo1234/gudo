ed57c550a58231e68f7a4792caa3bdc710b82d8d
gudo1234 <izumilitee@gmail.com>
2024-12-29 08:48:59 -0600

    Update welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -43,7 +43,7 @@ if (media === 'boton')
 conn.sendMessage(m.chat, {
            image: im,
            caption: text,
-           mentions: [user],
+           mentions: user,
            footer: wm,
            buttons: [{
              buttonId: ".trizte",

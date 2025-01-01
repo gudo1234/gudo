367a6e74f06304a28e5631df5aa7ecec2315a128
gudo1234 <izumilitee@gmail.com>
2025-01-01 11:47:19 -0600

    Update prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -210,9 +210,9 @@ conn.sendMessage(m.chat, {
 if (media === 'botons')
 conn.sendButtonMessages(m.chat, [
 [`⭐Hola ${taguser} *🥀Buenos días🌅/tardes🌇/noches🌆*\n\n${txt}`, '𝗉𝗈𝗐𝖾𝗋𝖾𝖽 ⓒ 𝖨𝗓𝗎𝗆𝗂-𝖻𝗈𝗍 𝟤𝟢𝟤𝟢-𝟤𝟢𝟤𝟦.', imagen4, [
-['Atom', usedPrefix + `atom`]
+['🚩ᴀᴛᴏᴍ', usedPrefix + `atom`]
 ], null, [
-['Canal', canal]],
+['🌐Canal', canal]],
 []
 ]], fkontak)
 }

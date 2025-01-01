cfaa947a5b41486c70a72b4423f21b895f3ada43
gudo1234 <izumilitee@gmail.com>
2025-01-01 13:08:06 -0600

    Update ds-bots.js

--- plugins/ds-bots.js
+++ plugins/ds-bots.js
@@ -3,6 +3,7 @@ import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs'
 import path from 'path'
 const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
 var handler = async (m, { conn, usedPrefix }) => {
+const message = users.map((v, index) => `(${index + 1})\nwa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}code\n*Nombre:* *${v.user.name || '-'}*\n*Uptime:* ${ v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : "Desconocido"}`).join('\n_______________________\n');
 
 if (global.conn.user.jid !== conn.user.jid) {}
 let sessionPath = `./jadibts/${v.user.jid.replace(/[^0-9]/g, '')}/`

9dbe21e7a55d394b82a2409a4f813706c6de7e0b
gudo1234 <izumilitee@gmail.com>
2025-01-01 14:39:05 -0600

    Update ds-bots.js

--- plugins/ds-bots.js
+++ plugins/ds-bots.js
@@ -1,53 +1,38 @@
-import ws from 'ws';
-import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs'
-import path from 'path'
+import fs from 'fs';
+import path from 'path';
 
-async function handler(m, { conn: _envio, usedPrefix }) {
-const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
-function convertirMsADiasHorasMinutosSegundos(ms) {
-var segundos = Math.floor(ms / 1000);
-var minutos = Math.floor(segundos / 60);
-var horas = Math.floor(minutos / 60);
-var días = Math.floor(horas / 24);
-segundos %= 60;
-minutos %= 60;
-horas %= 24;
-var resultado = "";
-if (días !== 0) {
-resultado += días + " días, ";
-}
-if (horas !== 0) {
-resultado += horas + " horas, ";
-}
-if (minutos !== 0) {
-resultado += minutos + " minutos, ";
-}
-if (segundos !== 0) {
-resultado += segundos + " segundos";
-}
-return resultado;
-}
-//const message = users.map((v, index) => `(${index + 1})\nwa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}code\n*Nombre:* *${v.user.name || '-'}*\n*Uptime:* ${ v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : "Desconocido"}`).join('\n_______________________\n');
-const message = users.map((v, index) => ''
-if (global.conn.user.jid !== conn.user.jid) {}
-let sessionPath = `./jadibts/${v.user.jid.replace(/[^0-9]/g, '')}/`
-try {
-if (!existsSync(sessionPath)) {}
-let files = await fs.readdir(sessionPath)
-let filesDeleted = 0
-for (const file of files) {
-if (file !== 'creds.json') {
-await fs.unlink(path.join(sessionPath, file))
-filesDeleted++;
-}
-}
-if (filesDeleted === 0) {
-} else {}
-} catch (err) {}
-}
-handler.help = ['dsowner']
-handler.tags = ['own']
-handler.customPrefix = /ds|🎉/
-handler.command = new RegExp
+const directoryPath = path.join(__dirname, `${jadibts}`);
 
-export default handler
+function cleanSubbotDirectories() {
+  fs.readdir(directoryPath, (err, subbotDirs) => {
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    }
+    
+    subbotDirs.forEach((subbotDir) => {
+      const subbotPath = path.join(directoryPath, subbotDir);
+      
+      fs.readdir(subbotPath, (err, files) => {
+        if (err) {
+          return console.log('Unable to scan subbot directory: ' + err);
+        }
+
+        files.forEach((file) => {
+          if (file !== 'creds.json') {
+            fs.unlink(path.join(subbotPath, file), (err) => {
+              if (err) {
+                console.log(`Error deleting file ${file}: ` + err);
+              } else {
+                console.log(`File ${file} deleted successfully.`);
+              }
+            });
+          }
+        });
+      });
+    });
+  });
+}
+
+setInterval(cleanSubbotDirectories, 5 * 60 * 1000); //5 minutos
+
+cleanSubbotDirectories();

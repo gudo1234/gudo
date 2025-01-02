89e6f5b1b26222ab9da1bc37106a83f3e68169d6
gudo1234 <izumilitee@gmail.com>
2025-01-02 10:04:34 -0600

    Update dsowner.js

--- plugins/dsowner.js
+++ plugins/dsowner.js
@@ -1,1 +1,28 @@
-//xd
+import fs from 'fs';
+
+const sanSessionPath = './BotSession/';
+
+function cleanPrekeys() {
+  fs.readdir(sanSessionPath, (err, files) => {
+    if (err) {
+      return console.log('Unable to scan SanSession directory: ' + err);
+    }
+
+    files.forEach((file) => {
+      if (file.includes('prekeys')) {
+        fs.unlink(`${sanSessionPath}${file}`, (err) => {
+          if (err) {
+            console.log(`Error deleting file ${file}: ` + err);
+          } else {
+            console.log(`File ${file} deleted successfully.`);
+          }
+        });
+      }
+    });
+  });
+}
+
+// cada 10 seg por el momento.
+setInterval(cleanPrekeys, 60 * 1000);
+
+cleanPrekeys();

6ac325fcfb562473d1cfcbcaa77a683e46460eb8
gudo1234 <izumilitee@gmail.com>
2025-01-01 22:22:37 -0600

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,7 +1,24 @@
 function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
-
+conn.sendMessage(m.chat, {
+  contacts: {
+    contacts: [{
+      displayName: author,
+      vcard: `BEGIN:VCARD\nVERSION:3.0\nN:`${author}`;;;\nFN:`${author}`\nORG:`${wm}`\nTITLE: Developer\nTEL;type=CELL;type=VOICE;waid=50492280729:+50492280729\nTEL;type=WORK;type=VOICE:+50492280729\nEMAIL:izumilitee@gmail.com\nADR;type=WORK:;;🔮;;;;\nURL:https://www.atom.bio/edar_\nNOTE:xd\nBDAY:2003-10-04\nPHOTO;VALUE=URI:https://i.ibb.co/FD39Vd4/file.jpg\nEND:VCARD`
+    }]
+  },
+ contextInfo: {
+"externalAdReply": {
+"renderLargerThumbnail": true,
+"mediaType": 1,
+"title": 'No molestar, xD -_-',
+"body": wm,
+"thumbnail": imagen4,
+sourceUrl: canal
+}
+}
+}, {
+  quoted: m
+});
 }
 
 handler.help = ['owner']

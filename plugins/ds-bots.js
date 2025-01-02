b1115e24b985e9314e90dd6b6765e648657030ca
gudo1234 <izumilitee@gmail.com>
2025-01-02 09:27:24 -0600

    Update ds-bots.js

--- plugins/ds-bots.js
+++ plugins/ds-bots.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const directoryPath = path.join(__dirname, `${jadibts}`);
+const directoryPath = path.join(__dirname, './jadibts/');
 
 function cleanSubbotDirectories() {
   fs.readdir(directoryPath, (err, subbotDirs) => {

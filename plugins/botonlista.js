a854c50fcf56bcdfb94b00f986cca365d462f257
gudo1234 <izumilitee@gmail.com>
2025-01-04 17:27:47 -0600

    Update botonlista.js

--- plugins/botonlista.js
+++ plugins/botonlista.js
@@ -12,7 +12,7 @@ if (m.isBaileys && m.fromMe)
        return !0
 //if (m.chat.endsWith('broadcast') || m.isGroup) return
 let user = global.db.data.users[m.sender]
-if (new Date() - user.pc < 43400000) return
+if (new Date() - user.pc < 360000) return
 conn.reply(m.chat, `¡Holaa ${taguser} Bienvenido a nuestro servicio de WhatsApp de Skynet, donde la electricidad cobra vida y te traemos todo lo que necesitas saber sobre el mundo eléctrico. Desde tips para ahorrar energía hasta soluciones para esos problemas eléctricos que te vuelven loco. ¡Estamos aquí para iluminar tus dudas! ⚡🔌 ¿En qué puedo ayudarte hoy?`, null)
 
 const { imageMessage } = await prepareWAMessageMedia({

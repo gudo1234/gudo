c131b58f7819ef590c64ffcb6370686127569b3a
gudo1234 <izumilitee@gmail.com>
2025-01-04 16:16:58 -0600

    Update botonlista.js

--- plugins/botonlista.js
+++ plugins/botonlista.js
@@ -3,8 +3,7 @@ import { randomBytes } from 'crypto';
 
 import moment from 'moment-timezone'
 export async function before(m, { conn, args, usedPrefix, command }) {
-if (m.text.includes('dueño') || m.text.includes('hola') || m.text.includes('ok'))
-       return !0
+if (m.sender === this.user.jid) { continue }
 if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup) return
 let user = global.db.data.users[m.sender]
 if (new Date() - user.pc < 180000) return

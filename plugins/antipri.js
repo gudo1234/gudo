b0ce95575287e13ff2194dc8d089a8af4769ab56
gudo1234 <izumilitee@gmail.com>
2025-01-04 08:24:01 -0600

    Update antipri.js

--- plugins/antipri.js
+++ plugins/antipri.js
@@ -1,4 +1,4 @@
-/*export async function before(m, { isAdmin, isBotAdmin, isOwner }) {
+export async function before(m, { isAdmin, isBotAdmin, isOwner }) {
     if (m.isBaileys && m.fromMe)
         return !0
     if (m.isGroup)
@@ -31,4 +31,4 @@ await this.sendFile(m.chat, vn, 'error.mp3', null,null, true, { type: 'audioMess
        await this.updateBlockStatus(m.chat, 'block')
     }
     return !1
-}*/
+}

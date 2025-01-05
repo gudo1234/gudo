0826c55beff21c4275caa734d2c743a28e2a053b
gudo1234 <izumilitee@gmail.com>
2025-01-04 18:33:45 -0600

    Update setppbot.js

--- plugins/setppbot.js
+++ plugins/setppbot.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
                 /*if (!m.isGroup) return
                 if (!isAdmins) return reply(mess.admin)
                 if (!isBotAdmins) return*/
-                if (!quoted) return
+                if (m.quoted) return
                 if (!/image/.test(mime)) return reply(`Send/Reply Image Caption Caption ${usedPrefix + command}`)
                 if (/webp/.test(mime)) return reply(`Send/Reply Image With Caption ${usedPrefix + command}`)
                 var medis = await conn.downloadAndSaveMediaMessage(quoted, 'ppbot.jpeg')

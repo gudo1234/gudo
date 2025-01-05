b6132323ac8ec7dc12f51f216cc7a0b61c1fdab3
gudo1234 <izumilitee@gmail.com>
2025-01-05 13:45:24 -0600

    Update tes2.js

--- plugins/tes2.js
+++ plugins/tes2.js
@@ -5,42 +5,9 @@ conn.reply(m.chat, `🚩 *Grupo Oficial de ${wm}*\nhttps://chat.whatsapp.com/KlF
 if (command == 'tes3')
 conn.reply(m.chat, `> 🤖 _Ademas te ofrecemos funciones necesarias para tus grupos, por ejemplo el antilink, antiarabe y bienvenida automática y muchos más, todo lo puedes encontrar en el .menu._`, m)
 
-if (command == 'tes4')
-conn.reply(m.chat, `Nuestro horario de atención es:
-
-⚡ Lunes a Viernes 
-*7:30 am a 8:00 pm*
-
-⚡ Sábados
-*7:30 am a 1:00 pm*`, m)
-conn.sendMessage(m.chat, {
-    image: {url: 'https://qu.ax/MePZP.jpg'},
-    caption: '🤗 ¿Hay algo más en que podamos ayudarle?',
-    footer: 'Seleccione opción:',
-    buttons: [
-      {
-        buttonId: ".tes5",
-        buttonText: {
-          displayText: "Si",
-        },
-        type: 1,
-      },
-      {
-        buttonId: "ok",
-        buttonText: {
-          displayText: "No",
-        },
-        type: 1,
-      },
-    ],
-    viewOnce: true,
-    headerType: 4,
-    mentions: [m.sender],
-  }, { quoted: fkontak});
-    
 if (command == 'tes5')
 conn.reply(m.chat, `¡Estamos aquí para ayudarte! 👋 Si tienes alguna pregunta o necesitas ayuda, selecciona una opción del menú anterior.`, m)
 }
 
-handler.command = ['tes2', 'tes3', 'tes4', 'tes5']
+handler.command = ['tes2', 'tes3', 'tes5']
 export default handler

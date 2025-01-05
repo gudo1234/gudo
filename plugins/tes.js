3de7014b64b1102d9c76aa6433c457da230cda96
gudo1234 <izumilitee@gmail.com>
2025-01-04 21:06:19 -0600

    Update tes.js

--- plugins/tes.js
+++ plugins/tes.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (command == 'tes')
 if (!text) return conn.reply(m.chat, `Texto faltante para el owner`, m, null, )
 let teks = `🗿 *Hola creador* ⭐El Numero Wa.me/${m.sender.split`@`[0]} Quiere de tus servicios`
-conn.reply('50247180167@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, { contextInfo: { mentionedJid: [m.sender] }})
+conn.reply('50488723207@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, { contextInfo: { mentionedJid: [m.sender] }})
   conn.reply(m.chat, `Por favor espere, nuestro siguiente asesor disponible le atenderá en breve`, m)
 
 if (command == 'tes2')

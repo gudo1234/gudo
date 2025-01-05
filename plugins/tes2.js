c9a76990b98f275b8441d197e42d2cc5112c4772
gudo1234 <izumilitee@gmail.com>
2025-01-05 14:03:04 -0600

    Update tes2.js

--- plugins/tes2.js
+++ plugins/tes2.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 if (command == 'tes2')
-conn.reply(m.chat, `🚩 *Grupo Oficial de ${wm}*\nhttps://chat.whatsapp.com/KlFxtwHtqIWIWOTjnjqnu3\n\n> 👍🏻Asegurese de leer las reglas para evitar expulsiones`, m)
+conn.reply(m.chat, `🚩 *Grupo Oficial:*\nhttps://chat.whatsapp.com/KlFxtwHtqIWIWOTjnjqnu3\n\n> 👍🏻Asegurese de leer las reglas para evitar expulsiones`, m)
 
 if (command == 'tes3')
 conn.reply(m.chat, `> 🤖 _Ademas te ofrecemos funciones necesarias para tus grupos, por ejemplo el antilink, antiarabe y bienvenida automática y muchos más, todo lo puedes encontrar en el .menu._`, m)

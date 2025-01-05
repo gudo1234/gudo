fdbeaaffb31fc04631e0f9a5d545efdaf467ccda
gudo1234 <izumilitee@gmail.com>
2025-01-04 21:20:34 -0600

    Update tes.js

--- plugins/tes.js
+++ plugins/tes.js
@@ -1,16 +1,8 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (command == 'tes')
 if (!text) return conn.reply(m.chat, `Texto faltante para el owner`, m, null, )
 let teks = `🗿 *Hola creador* ⭐El Numero Wa.me/${m.sender.split`@`[0]} Quiere de tus servicios`
 conn.reply('50247180167@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, { contextInfo: { mentionedJid: [m.sender] }})
-  conn.reply(m.chat, `Por favor espere, nuestro siguiente asesor disponible le atenderá en breve`, m)
-
-if (command == 'tes2')
-conn.reply(m.chat, `https://chat.whatsapp.com/J0LI68TsIghF3ldmsTgDWO`, m)
-
-if (command == 'tes3')
-conn.reply(m.chat, `tes3`, m)
 }
 
-handler.command = ['tes', 'tes2', 'tes3']
+handler.command = ['tes']
 export default handler

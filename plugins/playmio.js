be7de523314e3bbd0a2d80058b7ed02ae25b4e6b
gudo1234 <izumilitee@gmail.com>
2025-01-05 19:09:04 -0600

    Update playmio.js

--- plugins/playmio.js
+++ plugins/playmio.js
@@ -5,7 +5,7 @@ if (command == 'play') {
 if (!text) return conn.reply(m.chat, `*Ejemplo:* ${usedPrefix + command} https://youtu.be/yURRmWtbTbo?si=y-ZKAGxrvNGhB5Qe`,  m);
 const res = await fetch(`https://api.zenkey.my.id/api/download/ytmp3?apikey=zenkey&url=text`)
 let { result } = await res.json()
-await conn.sendMessage(m.chat, { audio: { url: text }, mimetype: 'audio/mpeg' }, { quoted: m })
+await conn.sendMessage(m.chat, { audio: { url: await result.download.url }, mimetype: 'audio/mpeg' }, { quoted: m })
 m.react('🕒')
 }
 }

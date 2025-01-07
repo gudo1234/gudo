5fd1b5682cf2b4bdc1c4b416971dc2d0182e488d
gudo1234 <izumilitee@gmail.com>
2025-01-07 17:30:34 -0600

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -1,19 +1,19 @@
-import moment from 'moment-timezone'
-import PhoneNum from 'awesome-phonenumber'
+import moment from 'moment-timezone';
+import PhoneNum from 'awesome-phonenumber';
 
-let regionNames = new Intl.DisplayNames(['en'], { type: 'region' })
+let regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
+
 let handler = async (m, { conn, usedPrefix, command }) => {
+  const format = new Intl.NumberFormat('en-EN'); // Puedes usar el locale que prefieras
 
-const format = new Intl.NumberFormat('en-EN'); // Puedes usar el locale que prefieras
-const regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
+  // Obtener el código de región internacional
+  let locale = format.resolvedOptions().locale; // Obtiene el locale completo
+  let regionCode = locale.includes('-') ? locale.split('-')[1] : locale; // Maneja el caso sin guion
 
-// Obtener el código de región internacional
-let regionCode = format.resolvedOptions().locale.split('-')[1]; // Esto es solo un ejemplo
-
-// Obtener el nombre del país
-let country = regionNames.of(regionCode);
-  conn.reply(m.chat, `*Pais:* ${country}`, m)
+  // Obtener el nombre del país
+  let country = regionNames.of(regionCode);
+  conn.reply(m.chat, `*País:* ${country}`, m);
 }
 
-handler.command = ['🍋‍🟩']
-export default handler
+handler.command = ['🍋‍🟩'];
+export default handler;

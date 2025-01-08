d6f4c560d347882fb96fde7d563d2fb5222657b2
gudo1234 <izumilitee@gmail.com>
2025-01-07 19:41:28 -0600

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -4,25 +4,21 @@ import PhoneNum from 'awesome-phonenumber';
 let regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-    // Suponiendo que el número de teléfono se pasa como argumento
-    let phoneNumber = m.text.split(' ')[1]; // Ejemplo: !coun +1234567890
+    let phoneNumber = m.text.split(' ')[1];
+
     if (!phoneNumber) {
-        return m.reply('Por favor, proporciona un número de teléfono. Ejemplo: !coun +1234567890');
+        return m.reply('Por favor, proporciona un número de teléfono. Ejemplo: ${usedPrefix + command} +1234567890');
     }
 
-    // Crear una instancia de PhoneNum
     let phone = new PhoneNum(phoneNumber);
     
-    // Obtener el país
     let countryCode = phone.getRegionCode();
     let countryName = regionNames.of(countryCode);
-    
-    // Obtener la bandera del país
+
     let flag = String.fromCodePoint(...[...countryCode].map(c => 127397 + c.charCodeAt(0)));
 
-    // Responder con el nombre del país y la bandera
     m.reply(`*País:* ${countryName} ${flag}`);
 }
 
-handler.command = ['co'];
+handler.command = ['coun'];
 export default handler;

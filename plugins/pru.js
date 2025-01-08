7924d4f95d038073e1fa918e6ea77b72ba3b14e0
gudo1234 <izumilitee@gmail.com>
2025-01-08 17:07:53 -0600

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -1,31 +1,15 @@
-import PhoneNum from 'libphonenumber-js';
+import moment from 'moment-timezone';
 
-let regionNames = new Intl.DisplayNames(['es'], { type: 'region' });
+let messageCount = 0;
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-    // Aquí asumimos que el número de teléfono se pasa como un argumento del comando
-    const phoneNumber = m.quoted[0]; // Suponiendo que el primer argumento es el número de teléfono
+    messageCount++;
 
-    if (!phoneNumber) {
-        return m.reply('*Por favor, proporciona un número de teléfono.*');
-    }
+    if (messageCount >= 50) {
+        messageCount = 0;
 
-    try {
-        const parsedNumber = PhoneNum.parsePhoneNumber(phoneNumber);
-        
-        if (parsedNumber) {
-            const countryCode = parsedNumber.country;
-            const countryName = regionNames.of(countryCode);
-            const flagEmoji = String.fromCodePoint(...[...countryCode].map(char => 127397 + char.charCodeAt(0)));
-
-            m.reply(`*País:* ${countryName} ${flagEmoji}`);
-        } else {
-            m.reply('*País:* No se pudo determinar el país.');
-        }
-    } catch (error) {
-        m.reply('*Error:* El número de teléfono no es válido.');
+        m.reply(`🗿`);
     }
 }
 
-handler.command = ['coun', '🍋‍🟩'];
 export default handler;

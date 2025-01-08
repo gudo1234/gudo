d1cac7315ff862631b364cd8cb98e38482fde105
gudo1234 <izumilitee@gmail.com>
2025-01-08 17:46:19 -0600

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -1,14 +1,15 @@
-import moment from 'moment-timezone';
-
 let messageCount = 0;
 
 let handler = async (m, { conn, usedPrefix, command }) => {
     messageCount++;
 
-    if (messageCount >= 5) {
-        messageCount = 0;
+    // Aquí puedes ajustar el límite según lo que desees
+    const limit = 3;
 
-        m.reply('xd');
+    if (messageCount >= limit) {
+        messageCount = 0; // Reiniciar el contador
+
+        await m.reply('xd js'); // Asegúrate de usar await si m.reply es una función asincrónica
     }
 }
 

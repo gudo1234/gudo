d889e1d0094c1ee499448a57cd2ca00c3c85b884
gudo1234 <izumilitee@gmail.com>
2025-01-07 21:49:18 -0600

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -1,4 +1,4 @@
-import PhoneNumber from 'awesome-phonenumber'
+/*import PhoneNumber from 'awesome-phonenumber'
 import fetch from 'node-fetch'
 
 var handler = async (m, { conn, participants, groupMetadata }) => {
@@ -35,5 +35,5 @@ let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
 let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [d, ' *Dias ☀️*\n ', h, ' *Horas 🕐*\n ', m, ' *Minutos ⏰*\n ', s, ' *Segundos ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
+return [d, ' *Dias ☀️*\n ', h, ' *Horas 🕐*\n ', m, ' *Minutos ⏰*\n ', s, ' *Segundos ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')*/
                                                                                                                                                                                                                                                                                                                                    }

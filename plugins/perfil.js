b932a1a0db3848ee70043e115481656f88e04354
gudo1234 <izumilitee@gmail.com>
2025-01-07 22:02:31 -0600

    Update perfil.js

--- plugins/perfil.js
+++ plugins/perfil.js
@@ -15,6 +15,9 @@ let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status
 	let business = await conn.getBusinessProfile(num)
 	let format = PhoneNum(`+${num.split('@')[0]}`)
 	let country = regionNames.of(format.getRegionCode('international'))
+	//xd
+	let phoneNumber = m.text.split(' ')[1];
+	let phone = new PhoneNum(phoneNumber);
 	let countryCode = phone.getRegionCode();
 	let flag = String.fromCodePoint(...[...countryCode].map(c => 127397 + c.charCodeAt(0)));
 	let wea = `\`WhatsApp Stalking\`\n\n*Pais :* ${country.toUpperCase()} ${flag}\n*Nombre :* ${name ? name : '-'}\n*Formato:* ${format.getNumber('international')}\n*Url:* wa.me/${num.split('@')[0]}\n*Tag :* @${num.split('@')[0]}\n*Bio :* ${bio?.status || '-'}\n*Bio Uptade:* ${bio?.setAt ? moment(bio.setAt.toDateString()).locale('id').format('LL') : '-'}\n\n${business ? `*Business Info 🫐*\n*BusinessId:* ${business.wid}\n*Website:* ${business.website ? business.website : '-'}\n*Email:* ${business.email ? business.email : '-'}\n*Categoria:* ${business.category}\n*Direccion:* ${business.address ? business.address : '-'}\n\*Zona Horaria:* ${business.business_hours.timezone ? business.business_hours.timezone : '-'}\n*Descripcion*: ${business.description ? business.description : '-'}` : '\`Cuenta De WhatsApp\`'}`

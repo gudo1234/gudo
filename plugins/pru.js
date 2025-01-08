2a398eacbfa1f5bb4157617e525a6acf70d8cddf
gudo1234 <izumilitee@gmail.com>
2025-01-07 22:16:54 -0600

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -3,18 +3,27 @@ import PhoneNum from 'libphonenumber-js';
 let regionNames = new Intl.DisplayNames(['es'], { type: 'region' });
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-    // Aquí obtendremos el número de teléfono del usuario
-    const phoneNumber = m.sender; // Suponiendo que m.sender contiene el número de teléfono
-    const parsedNumber = PhoneNum.parsePhoneNumber(phoneNumber);
-    
-    if (parsedNumber) {
-        const countryCode = parsedNumber.country;
-        const countryName = regionNames.of(countryCode);
-        const flagEmoji = String.fromCodePoint(...[...countryCode].map(char => 127397 + char.charCodeAt(0)));
+    // Aquí asumimos que el número de teléfono se pasa como un argumento del comando
+    const phoneNumber = m.args[0]; // Suponiendo que el primer argumento es el número de teléfono
 
-        m.reply(`*País:* ${countryName} ${flagEmoji}`);
-    } else {
-        m.reply('*País:* No se pudo determinar el país.');
+    if (!phoneNumber) {
+        return m.reply('*Por favor, proporciona un número de teléfono.*');
+    }
+
+    try {
+        const parsedNumber = PhoneNum.parsePhoneNumber(phoneNumber);
+        
+        if (parsedNumber) {
+            const countryCode = parsedNumber.country;
+            const countryName = regionNames.of(countryCode);
+            const flagEmoji = String.fromCodePoint(...[...countryCode].map(char => 127397 + char.charCodeAt(0)));
+
+            m.reply(`*País:* ${countryName} ${flagEmoji}`);
+        } else {
+            m.reply('*País:* No se pudo determinar el país.');
+        }
+    } catch (error) {
+        m.reply('*Error:* El número de teléfono no es válido.');
     }
 }
 

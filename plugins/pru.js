268a09269aa5c5bf30e9ba7b2e3ec4f81b60e47c
gudo1234 <izumilitee@gmail.com>
2025-01-07 22:12:48 -0600

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -1,1 +1,22 @@
+import PhoneNum from 'libphonenumber-js';
 
+let regionNames = new Intl.DisplayNames(['es'], { type: 'region' });
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+    // Aquí obtendremos el número de teléfono del usuario
+    const phoneNumber = m.sender; // Suponiendo que m.sender contiene el número de teléfono
+    const parsedNumber = PhoneNum.parsePhoneNumber(phoneNumber);
+    
+    if (parsedNumber) {
+        const countryCode = parsedNumber.country;
+        const countryName = regionNames.of(countryCode);
+        const flagEmoji = String.fromCodePoint(...[...countryCode].map(char => 127397 + char.charCodeAt(0)));
+
+        m.reply(`*País:* ${countryName} ${flagEmoji}`);
+    } else {
+        m.reply('*País:* No se pudo determinar el país.');
+    }
+}
+
+handler.command = ['coun', '🍋‍🟩'];
+export default handler;

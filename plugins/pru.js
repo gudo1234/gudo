a989d3448918c42727c5d34affc2f6e3be55daba
gudo1234 <izumilitee@gmail.com>
2025-01-07 19:55:48 -0600

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -4,14 +4,21 @@ import PhoneNum from 'awesome-phonenumber';
 let regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-    let phoneNumber = m.text.split(' ')[1];
+    // Verificar si hay un mensaje citado
+    let quotedMessage = m.quoted ? m.quoted : m;
+    let phoneNumber = quotedMessage.text; // Obtener el texto del mensaje citado
 
     if (!phoneNumber) {
-        return m.reply('Por favor, proporciona un número de teléfono. Ejemplo: !coun +1234567890');
+        return m.reply('Por favor, cita un mensaje que contenga un número de teléfono. Ejemplo: !coun +1234567890');
     }
 
     let phone = new PhoneNum(phoneNumber);
     
+    // Verificar si el número de teléfono es válido
+    if (!phone.isValid()) {
+        return m.reply('El número de teléfono proporcionado no es válido. Asegúrate de incluir el código de país.');
+    }
+
     let countryCode = phone.getRegionCode();
     let countryName = regionNames.of(countryCode);
 
@@ -20,5 +27,5 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     m.reply(`*País:* ${countryName} ${flag}`);
 }
 
-handler.command = ['coun'];
+handler.command = ['coun', '🍋‍🟩'];
 export default handler;

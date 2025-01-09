385a23d1feaeb96df51a7b69830304035ffcfd1a
gudo1234 <izumilitee@gmail.com>
2025-01-09 02:06:29 -0600

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -1,4 +1,3 @@
-import fetch from 'node-fetch'
 let userMessageCount = {}
 let flags = [
   {
@@ -25,23 +24,20 @@ let flags = [
 
 export async function before(m, { conn, args, usedPrefix, command }) {
   if (!m.message) return !0;
-    if (!userMessageCount[m.sender]) userMessageCount[m.sender] = { count: 0, currentFlag: null };
+  if (!userMessageCount[m.sender]) userMessageCount[m.sender] = { count: 0, currentFlag: null };
 
-    userMessageCount[m.sender].count += 1;
+  userMessageCount[m.sender].count += 1;
 
-    if (userMessageCount[m.sender].count % 10 === 0) {
-        // Elegir una bandera aleatoria
-        const randomFlag = flags[Math.floor(Math.random() * flags.length)];
-        userMessageCount[m.sender].currentFlag = randomFlag.name; // Guardar el país actual
-        userMessageCount[m.sender].currentFlag2 = randomFlag.emoji;
-        //await conn.sendFile(m.chat, img, "Thumbnail.jpg", `🌎 ¿A qué país pertenece esta bandera? ${userMessageCount[m.sender].currentFlag2}.`, null);
-    let img = imagen4
-    await conn.sendFile(m.chat, randomFlag.image, "Thumbnail.jpg", `¿A qué país pertenece esta bandera? ${userMessageCount[m.sender].currentFlag2}.`, m, null, rcanal)
-    }
+  if (userMessageCount[m.sender].count % 10 === 0) {
+    // Elegir una bandera aleatoria
+    const randomFlag = flags[Math.floor(Math.random() * flags.length)];
+    userMessageCount[m.sender].currentFlag = randomFlag.name; // Guardar el país actual
+    userMessageCount[m.sender].currentFlag2 = randomFlag.emoji;
 
-    // Detectar la respuesta del usuario
-    if (m.text.toLowerCase() === userMessageCount[m.sender].currentFlag.toLowerCase()) {
-        await conn.reply(m.chat, `¡Correcto, ${m.pushName}! 🎉 La bandera es de ${userMessageCount[m.sender].currentFlag}.`, m);
-        userMessageCount[m.sender].currentFlag = null; // Resetear el país actual
-    }
+    // Asegúrate de que el enlace sea válido
+    const imageUrl = randomFlag.image;
+    
+    // Enviar la imagen de la bandera
+    await conn.sendFile(m.chat, imageUrl, "Thumbnail.jpg", `¿A qué país pertenece esta bandera? ${userMessageCount[m.sender].currentFlag2}.`, m, null, rcanal);
+  }
 }

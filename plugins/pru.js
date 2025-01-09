15dd884aff75001f4a7c909a65b4bf5d2d04c665
gudo1234 <izumilitee@gmail.com>
2025-01-08 20:44:59 -0600

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -1,5 +1,5 @@
 let userMessageCount = {};
-let flags = require('./flags.json'); // Asegúrate de que el archivo flags.json esté en el mismo directorio
+let flags = require('./flags.json');
 
 export async function before(m, { conn, args, usedPrefix, command }) {
     if (!m.message) return !0;
@@ -8,18 +8,15 @@ export async function before(m, { conn, args, usedPrefix, command }) {
     userMessageCount[m.sender] += 1;
 
     if (userMessageCount[m.sender] % 10 === 0) {
-        // Elegir una bandera aleatoria del archivo flags.json
         let randomFlag = flags[Math.floor(Math.random() * flags.length)];
-        let img = randomFlag.hex_image; // Asegúrate de que tu objeto tenga una propiedad 'image' con la URL de la imagen
-        let country = randomFlag.country; // Asegúrate de que tu objeto tenga una propiedad 'country' con el nombre del país
+        let img = randomFlag.hex_image;
+        let country = randomFlag.country;
 
-        await conn.sendFile(m.chat, img, "Thumbnail.jpg", `¿De qué país es esta bandera?`, null);
+        await conn.sendFile(m.chat, img, "Thumbnail.jpg", `🕒 ¿De qué país es esta bandera?`, null);
 
-        // Esperar la respuesta del usuario
         conn.on('message', async (response) => {
             if (response.body.toLowerCase() === country.toLowerCase()) {
-                await conn.sendMessage(m.chat, `¡Correcto, ${m.sender.split('@')[0]}! 🎉 Es ${country}.`, null);
-                // Puedes agregar más lógica aquí si quieres
+                await conn.sendMessage(m.chat, `¡Correcto, ${m.sender.split('@')[0]}! 🎉 Es ${country}.`, m);
             } else {
                 await conn.sendMessage(m.chat, `Incorrecto, ${m.sender.split('@')[0]}! 😢 Intenta de nuevo.`, m);
             }

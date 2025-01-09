46ec62a06e46cff4df9d817c6c0f6b49dd7fc6df
gudo1234 <izumilitee@gmail.com>
2025-01-08 18:34:34 -0600

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -1,18 +1,14 @@
 let userMessageCount = {}
 
 export async function before(m, { conn, args, usedPrefix, command }) {
-    //if (m.fromMe) return
-    //if (m.isBaileys && m.fromMe) return !0
-
+    
     // Verificamos si el mensaje es de un grupo o privado
     if (!m.message) return !0
     if (!userMessageCount[m.sender]) userMessageCount[m.sender] = 0
 
-    // Incrementamos el contador de mensajes
-    userMessageCount[m.sender] += 1
+    userMessageCount[m.sender] += 1 //contador de mensajes
 
-    // Si el usuario ha enviado 5 mensajes, respondemos
-    if (userMessageCount[m.sender] % 10 === 0) {
-        conn.reply(m.chat, `xd`, null)
+    if (userMessageCount[m.sender] % 10 === 0) { //aqui se controla cada cuántos mensajes
+        conn.reply(m.chat, `${taguser}`, null)
     }
 }

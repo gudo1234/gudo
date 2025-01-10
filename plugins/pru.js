edc22cc674d853deecd9e401013eafebc7c62f38
gudo1234 <izumilitee@gmail.com>
2025-01-10 01:46:43 -0600

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -25,7 +25,7 @@ let flags = [
 
 export async function before(m, { conn, args, usedPrefix, command }) {
     if (!m.message) return !0;
-    if (!userMessageCount[m.chat]) userMessageCount[m.chat] = { count: 0, currentFlag: null };
+    if (!userMessageCount[m.chat]) userMessageCount[m.chat] = { count: 0, currentFlag: null, timeout: null };
 
     userMessageCount[m.chat].count += 1;
 
@@ -34,15 +34,24 @@ export async function before(m, { conn, args, usedPrefix, command }) {
         const randomFlag = flags[Math.floor(Math.random() * flags.length)];
         userMessageCount[m.chat].currentFlag = randomFlag.name; // Guardar el país actual
         userMessageCount[m.chat].currentFlag2 = randomFlag.emoji;
-let txt = `💣 ¿A qué país pertenece esta bandera? ${userMessageCount[m.chat].currentFlag2}.`
+        let txt = `💣 ¿A qué país pertenece esta bandera? ${userMessageCount[m.chat].currentFlag2}.`
+        
         await conn.sendFile(m.chat, randomFlag.image, "Thumbnail.jpg", txt, null);
+        
+        // Establecer un temporizador de 3 minutos
+        userMessageCount[m.chat].timeout = setTimeout(async () => {
+            await conn.reply(m.chat, `⏰ Tiempo agotado! La pregunta era: ${txt}`, m);
+            userMessageCount[m.chat].currentFlag = null; // Reiniciar la bandera actual
+        }, 180000); // 180000 ms = 3 minutos
     }
 
     // Detectar la respuesta del usuario
-if (m.text.toLowerCase() === userMessageCount[m.chat].currentFlag.toLowerCase() && m.quoted) {
-    await conn.reply(m.chat, `¡Correcto, ${m.pushName}! 🎉 La bandera es de ${userMessageCount[m.chat].currentFlag}.`, m);
-} else if (m.quoted) {
-    m.react('✖️');
-    await conn.reply(m.chat, `¡Respuesta Incorrecta!\n> vuelve a intentar`, m);
-}
+    if (m.text.toLowerCase() === userMessageCount[m.chat].currentFlag.toLowerCase() && m.quoted) {
+        clearTimeout(userMessageCount[m.chat].timeout); // Limpiar el temporizador si la respuesta es correcta
+        await conn.reply(m.chat, `¡Correcto, ${m.pushName}! 🎉 La bandera es de ${userMessageCount[m.chat].currentFlag}.`, m);
+        userMessageCount[m.chat].currentFlag = null; // Reiniciar la bandera actual
+    } else if (m.quoted) {
+        m.react('✖️');
+        await conn.reply(m.chat, `¡Respuesta Incorrecta!\n> vuelve a intentar`, m);
+    }
 }

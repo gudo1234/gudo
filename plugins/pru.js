cc9aef52caf6f5fbe9400d671403fd5e675b618b
gudo1234 <izumilitee@gmail.com>
2025-01-10 12:17:23 -0600

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -1,6 +1,5 @@
 import moment from 'moment-timezone';
 let userMessageCount = {};
-let userResponses = {}; // Para almacenar las respuestas de los usuarios
 let flags = [
   {
     "name": "Afghanistan",
@@ -26,7 +25,7 @@ let flags = [
 
 export async function before(m, { conn, args, usedPrefix, command }) {
     if (!m.message) return !0;
-    if (!userMessageCount[m.chat]) userMessageCount[m.chat] = { count: 0, currentFlag: null, questionMessage: null, timestamp: null, answeredBy: null };
+    if (!userMessageCount[m.chat]) userMessageCount[m.chat] = { count: 0, currentFlag: null, questionMessage: null, timestamp: null };
 
     userMessageCount[m.chat].count += 1;
 
@@ -36,10 +35,9 @@ export async function before(m, { conn, args, usedPrefix, command }) {
         userMessageCount[m.chat].currentFlag = randomFlag.name; // Guardar el país actual
         userMessageCount[m.chat].currentFlag2 = randomFlag.emoji;
 
-        let txt = `💣 *¿A qué país pertenece esta bandera? ${userMessageCount[m.chat].currentFlag2}.*\n_✍🏻Responda a este mensaje con la respuesta correcta_\n\n> 🕒Tiempo: 3 minutos`;
-        userMessageCount[m.chat].questionMessage = await conn.sendFile(m.chat, randomFlag.image, "Thumbnail.jpg", txt, null, null, rcanal);
+        let txt = `💣 ¿A qué país pertenece esta bandera? ${userMessageCount[m.chat].currentFlag2}\n_🤖Responde a este mensaje con la respuesta correcta_.\n> *Tiempo:* 3 minutos`;
+        userMessageCount[m.chat].questionMessage = await conn.sendFile(m.chat, randomFlag.image, "Thumbnail.jpg", txt, null);
         userMessageCount[m.chat].timestamp = Date.now(); // Guardar el tiempo de la pregunta
-        userMessageCount[m.chat].answeredBy = null; // Reiniciar el ID del usuario que respondió correctamente
     }
 
     // Detectar la respuesta del usuario
@@ -52,21 +50,13 @@ export async function before(m, { conn, args, usedPrefix, command }) {
         return; // No hacer nada más si el tiempo se ha agotado
     }
 
-    if (m.quoted && m.quoted.id === userMessageCount[m.chat].questionMessage.id) {
-        if (userMessageCount[m.chat].answeredBy) {
-            await conn.reply(m.chat, `*Esta pregunta ya fue respondida anteriormente por otro usuario* ¡Intenta más tarde!`, m);
-            return; // No permitir responder nuevamente si ya fue respondida
-        }
-
-        if (m.text.toLowerCase() === userMessageCount[m.chat].currentFlag.toLowerCase()) {
-            await conn.reply(m.chat, `*¡Correcto, ${m.pushName}!* 🎉 La bandera es de ${userMessageCount[m.chat].currentFlag}.`, m);
-            userMessageCount[m.chat].answeredBy = m.sender; // Guardar el ID del usuario que respondió correctamente
-            userMessageCount[m.chat].currentFlag = null; // Reiniciar la bandera actual
-            userMessageCount[m.chat].questionMessage = null; // Reiniciar el mensaje de la pregunta
-            userMessageCount[m.chat].timestamp = null; // Reiniciar la marca de tiempo
-        } else {
-            m.react('✖️');
-            await conn.reply(m.chat, `¡Respuesta Incorrecta!\n> vuelve a intentar`, m);
-        }
+    if (m.quoted && m.quoted.id === userMessageCount[m.chat].questionMessage.id && m.text.toLowerCase() === userMessageCount[m.chat].currentFlag.toLowerCase()) {
+        await conn.reply(m.chat, `¡Correcto, ${m.pushName}! 🎉 La bandera es de ${userMessageCount[m.chat].currentFlag}.`, m);
+        userMessageCount[m.chat].currentFlag = null; // Reiniciar la bandera actual
+        userMessageCount[m.chat].questionMessage = null; // Reiniciar el mensaje de la pregunta
+        userMessageCount[m.chat].timestamp = null; // Reiniciar la marca de tiempo
+    } else if (m.quoted && m.quoted.id === userMessageCount[m.chat].questionMessage.id) {
+        m.react('✖️');
+        await conn.reply(m.chat, `¡Respuesta Incorrecta!\n> vuelve a intentar`, m);
     }
 }

4312418c800d28ea610ada2507ab14dcfcd26e49
gudo1234 <izumilitee@gmail.com>
2025-01-10 12:18:42 -0600

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -36,7 +36,7 @@ export async function before(m, { conn, args, usedPrefix, command }) {
         userMessageCount[m.chat].currentFlag2 = randomFlag.emoji;
 
         let txt = `💣 ¿A qué país pertenece esta bandera? ${userMessageCount[m.chat].currentFlag2}\n_🤖Responde a este mensaje con la respuesta correcta_.\n> *Tiempo:* 3 minutos`;
-        userMessageCount[m.chat].questionMessage = await conn.sendFile(m.chat, randomFlag.image, "Thumbnail.jpg", txt, null);
+        userMessageCount[m.chat].questionMessage = await conn.sendFile(m.chat, randomFlag.image, "Thumbnail.jpg", txt, null, null, rcanal);
         userMessageCount[m.chat].timestamp = Date.now(); // Guardar el tiempo de la pregunta
     }
 

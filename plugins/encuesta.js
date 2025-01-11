adff90f04d487a1e49e0e37986a3d1beedf6b130
gudo1234 <izumilitee@gmail.com>
2025-01-11 01:32:04 -0600

    Update encuesta.js

--- plugins/encuesta.js
+++ plugins/encuesta.js
@@ -7,15 +7,16 @@ let handler = async (m, { conn, text, args, participants, usedPrefix, command })
     if (b.length > 14) return conn.reply(m.chat, `🚩 Lo máximo que debes poner es un total de *13* opciones.`, m)
 
     for (let c = 1; c < b.length; c++) { a.push([b[c]]) }
-    let texto = `📊 *Encuesta creada por:* ${conn.getName(m.sender)}\n*${text.split('|')[0]}*`
-
+    
     // Aquí creamos las menciones
     let groupMetadata = await conn.groupMetadata(m.chat);
-const mentions = groupMetadata.participants.map(v => v.id);
-const message = texto + '\n' + mentions.map(id => `@${id.split('@')[0]}`).join(', '); // Formateamos las menciones
+    const mentions = groupMetadata.participants.map(v => v.id);
+    
+    // Modificamos la línea de texto para incluir las menciones
+    let texto = `🚩 *Encuesta creada por:* ${conn.getName(m.sender)}\n*${text.split('|')[0]}*\n${mentions.map(id => `@${id.split('@')[0]}`).join(', ')}`;
 
-// Aquí se envía el mensaje con las menciones
-return conn.sendPoll(m.chat, message, a, { mentions, quoted: m });
+    // Aquí se envía el mensaje con las menciones
+    return conn.sendPoll(m.chat, texto, a, { mentions, quoted: m });
 }
 handler.command = ['poll', 'encuesta', 'crearencuesta', 'startpoll', 'encuestas', 'polls'] 
 export default handler

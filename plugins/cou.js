7f49dd7ad343949e244c597b1788f8d6823078fd
gudo1234 <izumilitee@gmail.com>
2025-01-11 12:45:59 -0600

    Update cou.js

--- plugins/cou.js
+++ plugins/cou.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, text, args, participants, usedPrefix, command }) => {
       try {
         await m.react('🕐')
-        if (!text) {
+        /*if (!text) {
           return conn.reply(m.chat, `🤨 *Ejemplo:* titulo|opcion 1|opcion 2`, m);
         }
-        /*let [name, ...value] = core.text.split`|`
+        let [name, ...value] = core.text.split`|`
             value = (value).join`|`,
             u = value.split`|`
         if (!value) {
@@ -24,7 +24,7 @@ let handler = async (m, { conn, text, args, participants, usedPrefix, command })
 let a = []
 let b = text.split('|')
 
-//if (!b[0]) return conn.reply(m.chat, `🚩 *Crea una encuesta.*\n*ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m) 
+if (!b[0]) return conn.reply(m.chat, `🚩 *Crea una encuesta.*\n*ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m) 
 if (!b[1]) return conn.reply(m.chat, `🚩 _Use de la siguiente forma utilizando_ *" | "* \n*Ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m)
 if (b[13]) return conn.reply(m.chat, `🚩 Lo máximo que debes poner es un total de *13* opciones.`, m)
         const poll = {

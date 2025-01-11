e98cb269ef44fb57ed0a343d68263a9a8c9e248c
gudo1234 <izumilitee@gmail.com>
2025-01-11 00:20:45 -0600

    Update encuesta.js

--- plugins/encuesta.js
+++ plugins/encuesta.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, args, participants, usedPrefix, command })
 let a = []
 let b = text.split('|')
 
-if (!b[0]) return conn.reply(m.chat, `🚩 Crea una encuesta, *ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m) 
+if (!b[0]) return conn.reply(m.chat, `🚩 *Crea una encuesta.*\n*ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m) 
 if (!b[1]) return conn.reply(m.chat, `🚩 _Use de la siguiente forma utilizando_ *" | "*\n> *Ejemplo:* *${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m)
 if (b[13]) return conn.reply(m.chat, `🚩 Lo máximo que debes poner es un total de *13* opciones.`, m)
 

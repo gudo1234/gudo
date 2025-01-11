6df4a5bb49c6f7166c10b8d459579011adcdb16f
gudo1234 <izumilitee@gmail.com>
2025-01-11 00:28:46 -0600

    Update encuesta.js

--- plugins/encuesta.js
+++ plugins/encuesta.js
@@ -1,14 +1,18 @@
 let handler = async (m, { conn, text, args, participants, usedPrefix, command }) => {	
-let a = []
-let b = text.split('|')
+    let a = []
+    let b = text.split('|')
 
-if (!b[0]) return conn.reply(m.chat, `🚩 *Crea una encuesta.*\n*ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m) 
-if (!b[1]) return conn.reply(m.chat, `🚩 _Use de la siguiente forma utilizando_ *" | "* \n*Ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m)
-if (b[13]) return conn.reply(m.chat, `🚩 Lo máximo que debes poner es un total de *13* opciones.`, m)
+    if (!b[0]) return conn.reply(m.chat, `🚩 *Crea una encuesta.*\n*ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m) 
+    if (!b[1]) return conn.reply(m.chat, `🚩 _Use de la siguiente forma utilizando_ *" | "* \n*Ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m)
+    if (b[13]) return conn.reply(m.chat, `🚩 Lo máximo que debes poner es un total de *13* opciones.`, m)
 
-for (let c = 1; c < b.length; c++) { a.push([b[c]]) }
-let texto = `📊 *Encuesta creada por:* ${conn.getName(m.sender)}\n*${text.split('|')[0]}*`
+    for (let c = 1; c < b.length; c++) { a.push([b[c]]) }
+    let texto = `📊 *Encuesta creada por:* ${conn.getName(m.sender)}\n*${text.split('|')[0]}*`
 
-return conn.sendPoll(m.chat, texto, a, {mentions: m})}
+    // Obtener las menciones de todos los participantes
+    let mentions = participants.map(p => p.id)
+    
+    return conn.sendPoll(m.chat, texto, a, { mentions })
+}
 handler.command = ['poll', 'encuesta', 'crearencuesta', 'startpoll', 'encuestas', 'polls'] 
 export default handler

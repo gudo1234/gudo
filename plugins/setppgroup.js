78f70e85f32ba02f287550117e461844fbd0c0ea
gudo1234 <izumilitee@gmail.com>
2025-01-11 17:35:18 -0600

    Update setppgroup.js

--- plugins/setppgroup.js
+++ plugins/setppgroup.js
@@ -1,25 +1,14 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
-    let q = m.quoted ? m.quoted : m;
-    let mime = q.mimetype || q.mediaType || '';
-
-    if (/image/.test(mime)) {
-        try {
-            let img = await q.download();
-            if (!img) throw `🚩 *Responda a una imagen*`;
-
-            await conn.updateProfilePicture(m.chat, img);
-            m.reply(`🍋‍🟩 _*Foto de perfil del grupo actualizada*_`);
-            m.react('✅');
-        } catch (error) {
-            console.error(error);
-            m.reply(`🚩 *Error al actualizar la foto de perfil: ${error.message}*`);
-        }
-    } else {
-        m.reply(`🚩 *Responda a una imagen*`);
-    }
-}
-
-handler.command = ['setppgc', 'setppgroup', 'icongc'];
-handler.group = true;
-handler.admin = true;
-handler.botAdmin = true;
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || q.mediaType || ''
+if (/image/.test(mime)) {
+let img = await q.download()
+if (!img) throw `🚩 *Responda a una imagen*`
+await conn.updateProfilePicture(m.chat, img).then(_ => m.reply(`🍋‍🟩 _*Foto de perfil del grupo actualizada_`))
+m.react('✅')
+} else throw `🚩 *Responda a una imagen*`}
+handler.command = ['setppgc', 'setppgroup', 'icongc']
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+export default handler

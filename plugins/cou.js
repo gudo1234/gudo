4dfbceae3785ad9a5a20b8b139cd5f45a7a888ea
gudo1234 <izumilitee@gmail.com>
2025-01-11 13:12:38 -0600

    Update cou.js

--- plugins/cou.js
+++ plugins/cou.js
@@ -1,37 +1,19 @@
-let handler = async (m, { conn, text, core, participants, usedPrefix, command }) => {
-      try {
-        await m.react('🕐')
-        if (!text) {
-          return conn.reply(m.chat, `🤨 *Ejemplo:* titulo|opcion 1|opcion 2`, m);
-        }
-        let [name, ...value] = text.split`|`
-            value = (value).join`|`,
-            u = text.split`|`
-            //u = []
-        if (!value) {
-          return conn.reply(m.chat, `Debe agregar más opciones, por *ejemplo:* ${usedPrefix + command} ¿Soy un buen bot?|sí|no|quizás`, m);
-        }
-        if (u.length < 2) {
-          return conn.reply(m.chat, `No puede crear una encuesta con menos de 2 opciones, intente hacerlo nuevamente con una mayor cantidad de opciones disponibles.`, m);
-        }
-        if (!m.isOwner) {
-          if (u.length > 5) {
-            return conn.reply(m.chat, `El número máximo de opciones para agregar a una encuesta es 5.`, m);
-          }
-        }
-        if (functions.hasDuplicates(u)) {
-          return conm.reply(m.chat, `Parece que algunas opciones están duplicadas.`, m);
-        } // aquí 
-/*let a = []
-//let b = text.split('|')
+let handler = async (m, { conn, text, args, participants, usedPrefix, command }) => {	
+let a = []
+let b = text.split('|')
 
 if (!b[0]) return conn.reply(m.chat, `🚩 *Crea una encuesta.*\n*ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m) 
 if (!b[1]) return conn.reply(m.chat, `🚩 _Use de la siguiente forma utilizando_ *" | "* \n*Ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m)
-if (b[13]) return conn.reply(m.chat, `🚩 Lo máximo que debes poner es un total de *13* opciones.`, m)*/
-        const poll = {
-          name: [name],
+if (b[13]) return conn.reply(m.chat, `🚩 Lo máximo que debes poner es un total de *13* opciones.`, m)
+
+for (let c = 1; c < b.length; c++) { a.push([b[c]]) }
+let texto = `📊 *Encuesta creada por:* ${conn.getName(m.sender)}\n*${text.split('|')[0]}*`
+
+//return conn.sendPoll(m.chat, texto, a, {mentions: m})}
+const poll = {
+          name: b,
           selectableCount: 1,
-          values: u,
+          values: a,
           mentions: m.participants,
           remoteJid: m.chat
         }  
@@ -40,7 +22,6 @@ if (b[13]) return conn.reply(m.chat, `🚩 Lo máximo que debes poner es un tota
           m.reply(`${error}`)
             console.error("Error al eliminar el mensaje:", error);
         }
-  }
 
-handler.command = ['no'] 
+handler.command = ['poll', 'encuesta', 'poll'] 
 export default handler

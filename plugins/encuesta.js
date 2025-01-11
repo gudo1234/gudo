d9bfc3813cec247951fb3dd1a7fcd6d88d498495
gudo1234 <izumilitee@gmail.com>
2025-01-11 11:48:35 -0600

    Update encuesta.js

--- plugins/encuesta.js
+++ plugins/encuesta.js
@@ -1,18 +1,3 @@
-/*let handler = async (m, { conn, text, args, participants, usedPrefix, command }) => {	
-let a = []
-let b = text.split('|')
-
-if (!b[0]) return conn.reply(m.chat, `🚩 *Crea una encuesta.*\n*ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m) 
-if (!b[1]) return conn.reply(m.chat, `🚩 _Use de la siguiente forma utilizando_ *" | "* \n*Ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m)
-if (b[13]) return conn.reply(m.chat, `🚩 Lo máximo que debes poner es un total de *13* opciones.`, m)
-
-for (let c = 1; c < b.length; c++) { a.push([b[c]]) }
-let texto = `📊 *Encuesta creada por:* ${conn.getName(m.sender)}\n*${text.split('|')[0]}*`
-
-return conn.sendPoll(m.chat, texto, a, {mentions: m})}
-handler.command = ['poll', 'encuesta', 'crearencuesta', 'startpoll', 'encuestas', 'polls'] 
-export default handler*/
-
 let handler = async (m, { conn, text, args, participants, usedPrefix, command }) => {	
     let a = []
     let b = text.split('|')
@@ -21,16 +6,23 @@ let handler = async (m, { conn, text, args, participants, usedPrefix, command })
     if (!b[1]) return conn.reply(m.chat, `🚩 _Use de la siguiente forma utilizando_ *" | "* \n*Ejemplo:* ${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....`, m)
     if (b.length > 13) return conn.reply(m.chat, `🚩 Lo máximo que debes poner es un total de *13* opciones.`, m)
 
-    for (let c = 1; c < b.length; c++) { a.push([b[c]]) }
+    for (let c = 1; c < b.length; c++) { 
+        a.push([b[c]]) 
+    }
+    
     let texto = `📊 *Encuesta creada por:* ${conn.getName(m.sender)}\n*${text.split('|')[0]}*`
+
+    // Aquí se obtienen las menciones de los participantes
+    const mentions = participants.map(participant => participant.id)
 
     const poll = {
         name: [b[0]], // Usando el primer texto como nombre de la encuesta
         selectableCount: 1,
         values: a,
-        mentions: m.participants,
+        mentions: mentions, // Aquí se corrige para usar las menciones correctamente
         remoteJid: m.chat
     }  
+
     return await conn.sendPoll(m.chat, texto, a, { poll })
 }
 

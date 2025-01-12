b163bdee0a49127377045c96d276f5f1d9d6c585
gudo1234 <izumilitee@gmail.com>
2025-01-11 22:17:36 -0600

    Create _fa.js

---
+++ plugins/_fa.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+if (!text) return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
+
+try {
+let api = await fetch(`https://api.dorratz.com/ytdl/yt-mp4?url==${text}`)
+let json = await api.json()
+let { title, views, likes, description, author } = json.metadata
+let HS = `- *Titulo :* ${title}
+- *Descripcion :* ${description}
+- *Visitas :* ${views}
+- *Likes :* ${likes}
+- *Autor :* ${author}
+- *Tamaño :* ${json.downloads.size}
+`
+m.reply(HS)
+await conn.sendMessage(m.chat, { audio: { url: json.downloads.url }, mimetype: 'audio/mpeg' }, { quoted: m });
+} catch (error) {
+console.error(error)
+}}
+
+handler.command = ['pla']
+
+export default handler

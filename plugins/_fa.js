1d8e35eaaefa1b9b4cfebf44a61da6603dbcc578
gudo1234 <izumilitee@gmail.com>
2025-01-11 23:35:09 -0600

    Update _fa.js

--- plugins/_fa.js
+++ plugins/_fa.js
@@ -2,18 +2,27 @@ import axios from "axios"
 
 let handler = async (m, { conn, text, usedPrefix, command, args }) => {
 if (!args[0]) return m.reply("Ingresa el enlace del vídeo de YouTube")
-
-
-    try { 
-        await m.react('⌛')
-    let name = await conn.getName(m.sender)
+let name = await conn.getName(m.sender)
+await m.react('⌛')
+    
+if (command == 'pla') {
+    try {
 await
     await conn.sendMessage(m.chat, { video: { url: `https://api.dorratz.com/ytdl/yt-mp4?url=${text}` }, caption: `${name}` }, { quoted: m });
 }catch(e) {
         await m.react('❌')
         return console.log(e)
-    }
+    }}
+
+if (command == 'pla4') {
+    try {
+await
+    await conn.sendMessage(m.chat, { document: { url: `https://api.dorratz.com/ytdl/yt-mp4?url=${text}` }, caption: `${name}` }, { quoted: m });
+}catch(e) {
+        await m.react('❌')
+        return console.log(e)
+    }}
 }
 
-handler.command = ['pla'];
+handler.command = ['pla', 'pla4'];
 export default handler;

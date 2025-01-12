db44c460f7a169bf77674e82ac6b13df506b8f07
gudo1234 <izumilitee@gmail.com>
2025-01-11 23:24:25 -0600

    Update _fa.js

--- plugins/_fa.js
+++ plugins/_fa.js
@@ -6,7 +6,9 @@ if (!args[0]) return m.reply("Ingresa el enlace del vídeo de YouTube")
 
     try { 
         await m.react('⌛')
-    await conn.sendMessage(m.chat, { video: { url: `https://api.dorratz.com/ytdl/yt-mp4?url=${text}` }, caption: taguser }, { quoted: m });
+    let name = await conn.getName(m.sender)
+await
+    await conn.sendMessage(m.chat, { video: { url: `https://api.dorratz.com/ytdl/yt-mp4?url=${text}` }, caption: `${name}` }, { quoted: m });
 }catch(e) {
         await m.react('❌')
         return console.log(e)

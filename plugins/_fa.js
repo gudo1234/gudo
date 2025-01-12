0c08f7fd217eb7cb8a31d08d4da97a1bd1b46bfa
gudo1234 <izumilitee@gmail.com>
2025-01-12 00:25:34 -0600

    Update _fa.js

--- plugins/_fa.js
+++ plugins/_fa.js
@@ -5,16 +5,16 @@ if (!args[0]) return m.reply("Ingresa el enlace del vídeo de YouTube")
 let name = await conn.getName(m.sender)
 await m.react('⌛')
     
-if (command == 'pla') {
+if (command == 'pla') { // yta audio
     try {
 await
-    await conn.sendMessage(m.chat, { document: { url: `https://api.dorratz.com/ytdl/yt-mp4?url=${text}` }, mimetype: 'video/mp4',fileName: `video.mp4`,caption: `${name}` }, { quoted: m });
+    await conn.sendMessage(m.chat, { audio: { url: `https://api.dorratz.com/ytdl/yt-mp4?url=${text}` }, mimetype: 'audio/mpeg'}, { quoted: m });
 }catch(e) {
         await m.react('❌')
         return console.log(e)
     }}
 
-if (command == 'pla4') {
+if (command == 'pla2') { // mp4 video
     try {
 await
     await conn.sendMessage(m.chat, { video: { url: `https://api.dorratz.com/ytdl/yt-mp4?url=${text}` }, caption: `${name}` }, { quoted: m });
@@ -22,7 +22,25 @@ await
         await m.react('❌')
         return console.log(e)
     }}
+
+if (command == 'pla3') { // ytadoc audio
+    try {
+await
+    await conn.sendMessage(m.chat, { document: { url: `https://api.dorratz.com/ytdl/yt-mp4?url=${text}` }, mimetype: 'audio/mpeg',fileName: `audio.mp3`,caption: `${name}` }, { quoted: m });
+}catch(e) {
+        await m.react('❌')
+        return console.log(e)
+    }}
+
+if (command == 'pla4') { // mp4doc video
+    try {
+await
+    await conn.sendMessage(m.chat, { document: { url: `https://api.dorratz.com/ytdl/yt-mp4?url=${text}` }, mimetype: 'video/mp4',fileName: `video.mp4`,caption: `${name}` }, { quoted: m });
+}catch(e) {
+        await m.react('❌')
+        return console.log(e)
+    }}
 }
 
-handler.command = ['pla', 'pla4'];
+handler.command = ['pla', 'pla2', 'pla3', 'pla4'];
 export default handler;

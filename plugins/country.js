c1d4efbe299a51509de825a23e91a67390b5bb3e
gudo1234 <izumilitee@gmail.com>
2025-01-12 23:58:18 -0600

    Update country.js

--- plugins/country.js
+++ plugins/country.js
@@ -2376,6 +2376,15 @@ export async function before(m, { conn, args, usedPrefix, command }) {
         let txt = `💣 *¿A qué país pertenece la bandera que se muestra? ${userMessageCount[m.chat].currentFlag2}*\n_🤖 Por favor, responda a este mensaje con la respuesta correcta en un plazo de *3 minutos*._`;
         userMessageCount[m.chat].questionMessage = await conn.sendFile(m.chat, randomFlag.image, "Thumbnail.jpg", txt, null, null, rcanal);
         userMessageCount[m.chat].timestamp = Date.now(); // Guardar el tiempo de la pregunta
+    const emojis = ['🔟', '9️⃣', '8️⃣', '7️⃣', '6️⃣', '5️⃣', '4️⃣', '3️⃣', '2️⃣', '1️⃣', '0️⃣', '🚨', '✖️'];
+const totalDuration = 180 * 1000; // 3 minutos en milisegundos
+const interval = totalDuration / emojis.length; // Tiempo entre cada emoji
+
+for (let i = 0; i < emojis.length; i++) {
+    setTimeout(async () => {
+        await m.react(emojis[i]);
+    }, i * interval);
+    }
     }
 
     // Detectar la respuesta del usuario

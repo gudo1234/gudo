a8aadb9187bcfd78da6e6726423dfe31d96352db
gudo1234 <izumilitee@gmail.com>
2025-01-12 18:30:59 -0600

    Update _fo.js

--- plugins/_fo.js
+++ plugins/_fo.js
@@ -1,11 +1,9 @@
 let handler = async (m) => {
     const emojis = ['✅', '😩', '😁', '😆', '😍', '😂', '🪀'];
-
     for (let i = 0; i < emojis.length; i++) {
-        // Usamos setTimeout para esperar un segundo antes de reaccionar
         setTimeout(async () => {
             await m.react(emojis[i]);
-        }, i * 1000); // Multiplicamos el índice por 1000 para el tiempo en milisegundos
+        }, i * 1000);
     }
 }
 

d5be9360a22e3f69c79e74b2ed48cdd8e2c99256
gudo1234 <izumilitee@gmail.com>
2025-01-13 10:24:35 -0600

    Update welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -20,9 +20,18 @@ let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch
   let or = ['stiker', 'audio', 'boton'];
   let media = or[Math.floor(Math.random() * 3)]
   let stiker = await sticker(imagen7, false, global.packname, global.author)
+  
+  const emojis = ['🍎', '🍒', '🍉', '🍊', '🍋', '🍏', '🍌', '🍍', '🍓', '🍇', '🍈', '🍒', '🍑', '🥭', '🍐', '🥥', '🍋‍🟩', '🌚'];
+  
   if (chat.welcome && (m.messageStubType === 28 || m.messageStubType === 32)) {
 if (media === 'stiker')
 this.sendFile(m.chat, stiker, 'sticker.webp', '',null, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `👋🏻ADIOS +${m.messageStubParameters[0].split`@`[0]}`, body: 'Esperemos que no vuelva -_-', mediaType: 2, sourceUrl: 'https://whatsapp.com/channel/0029VaXHNMZL7UVTeseuqw3H', thumbnail: im}}}, { quoted: null })
+
+for (let i = 0; i < emojis.length; i++) {
+                setTimeout(async () => {
+                    await m.react(emojis[i]);
+                }, i * 1000);
+            }
 
 if (media === 'audio')
 this.sendMessage(m.chat, { audio: { url: vn2 }, 
@@ -38,6 +47,12 @@ this.sendMessage(m.chat, { audio: { url: vn2 },
     "sourceUrl": 'https://www.atom.bio/edar_', 
     "showAdAttribution": true}}, 
      seconds: '4556', ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+
+    for (let i = 0; i < emojis.length; i++) {
+                setTimeout(async () => {
+                    await m.react(emojis[i]);
+                }, i * 1000);
+    }
 
 if (media === 'boton')
 conn.sendMessage(m.chat, {
@@ -64,5 +79,12 @@ conn.sendMessage(m.chat, {
     headerType: 4,
     mentions: [m.sender],
   }, { quoted: fkontak});
+
+for (let i = 0; i < emojis.length; i++) {
+                setTimeout(async () => {
+                    await m.react(emojis[i]);
+                }, i * 1000);
+            }
+  
   }
       }

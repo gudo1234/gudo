8d1002f930bedb1800071d21b106630ea8cea085
gudo1234 <izumilitee@gmail.com>
2025-01-13 18:06:35 -0600

    Update prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,6 +1,5 @@
 import { getDevice } from "@whiskeysockets/baileys"
 import PhoneNumber from 'awesome-phonenumber'
-import { unwatchFile, watchFile } from 'fs'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
@@ -21,9 +20,8 @@ let name = await conn.getName(m.sender)
   let paisdata = delirius.data.result
   let mundo = paisdata ? `${paisdata.name} ${paisdata.emoji}` : 'Desconocido'*/
 m.react('🚦')
-//let fs = require('fs');
-let or = ['evento', 'anuncio', 'boton', 'botons', 'document'];
-  let media = or[Math.floor(Math.random() * 5)]
+let or = ['evento', 'anuncio', 'boton', 'botons'];
+  let media = or[Math.floor(Math.random() * 4)]
 let tx = `°∧,,,∧
 (  ̳• · • ̳)
 /    づ♡ *_вιєиνєиι∂σ α ℓσѕ ¢σмαи∂σѕ_*\n> ⭐Hola *${name}*\n╭──┄┄─◌──┄┄ 🍋‍🟩 ̥˚◌\n*🥀Buenos días🌅tardes🌇noches🌆*\n╰── ── •◌• ── ─ 🔖‧₊˚`
@@ -226,28 +224,7 @@ conn.sendButtonMessages(m.chat, [
 []
 ]], fkontak)
 
-if (media === 'document')
-conn.sendMessage(m.chat, {
-    document: fs.readFileSync('./media/Menu2.jpg'),
-    fileName: `${await conn.getName(m.chat)}`,
-    mimetype: 'application/pdf',
-    caption: `${tx}\n\n${txt}`,
-    contextInfo: {
-        externalAdReply: {
-            title: wm,
-            body: `${m.pushName}`,
-            mediaType: 1,
-            previewType: 0,
-            showAdAttribution: true,
-            renderLargerThumbnail: true,
-            thumbnail: imagen4,
-            thumbnailUrl: imagen4,
-            sourceUrl: canal
-        }
-    }
-})
 }
-
 handler.command = ['menu', 'menú', 'memu', 'memú', 'help', 'comandos', 'ayuda', 'commands', 'commandos']
 handler.group = true;
 export default handler

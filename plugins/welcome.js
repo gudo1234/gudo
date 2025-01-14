0801b5482eb0f11b07c5e2a0d077cc4d4a9f9250
gudo1234 <izumilitee@gmail.com>
2025-01-13 22:53:49 -0600

    Update welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -23,7 +23,7 @@ let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch
 
 // Welcome 
 if (chat.welcome && m.messageStubType == 27) {
-  let wel = `°    /)🎩/)
+  let wel = `    /)🎩/)
     (｡•ㅅ•｡)𖹭︩︪𝚆꯭᪶۫۫͝𝙴꯭᪶͡𝙻᪶۫۫͝𝙲꯭᪶֟፟፝͡𝙾᪶۫۫͝𝙼꯭᪶͡𝙴᪶𖹭︩︪*
     ╭∪─∪─────────❤︎₊᪲
 ¡Hola!🍷 *@${m.messageStubParameters[0].split`@`[0]}* buenos días/tardes/noches.\n🎉¡Bienvenido a *${groupMetadata.subject}*!\n\n> 🐢Disfruta del grupo, diviértete, no olvides en leer las reglas...

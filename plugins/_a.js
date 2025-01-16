e48f8dd02bbee3f5892c0b0e2eae6522c35a64d6
gudo1234 <izumilitee@gmail.com>
2025-01-15 22:14:58 -0600

    Update _a.js

--- plugins/_a.js
+++ plugins/_a.js
@@ -5,7 +5,7 @@ handler.command = ['a'];
 export default handler;*/
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  await conn.sendEvent(m.chat, 'test', 'test2', "99999999999999999999999999999999999999999999", { quoted: m });
+  await conn.sendEvent(m.chat, 'puto', 'test2', "99999999999999999999999999999999999999999999", { quoted: m });
 }
 handler.command = ['a'];
 export default handler;

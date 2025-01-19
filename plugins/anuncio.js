67c0f444f0e057c21456c457a493f7c08c3c6096
gudo1234 <izumilitee@gmail.com>
2025-01-18 18:30:16 -0600

    Update anuncio.js

--- plugins/anuncio.js
+++ plugins/anuncio.js
@@ -1,6 +1,5 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-let name = await conn.getName(m.sender)
-await conn.sendMessage(m.chat, { text: global.espere + `*${name}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, thumbnailUrl: img.getRandom(), thumbnail: img.getRandom(), showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
+await conn.sendMessage(m.chat, { text: global.espere + `*${m.pushName}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, thumbnailUrl: img.getRandom(), thumbnail: img.getRandom(), showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
 }
 handler.command = ['ve']
 export default handler

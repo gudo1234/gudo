7ca1d8c651cf9316cc7f222e774b7522fcd26e33
gudo1234 <izumilitee@gmail.com>
2025-01-19 17:55:32 -0600

    Update playmio.js

--- plugins/playmio.js
+++ plugins/playmio.js
@@ -18,14 +18,14 @@ let { title, thumbnail, timestamp, views, ago, url } = video
 
 let vistas = parseInt(views).toLocaleString("es-ES") + " vistas"
 m.react('🕒')
-let HS = `_*⚡YouTube-Play⚡*_\n\n*Duración:*${timestamp}
+
+if (command == 'play') {
+let HS = `_*⚡YouTube-Play⚡*_\n\n*Duración:* ${timestamp}
 *Vistas*: ${vistas}
 *Uploaded:* ${ago}
 *Url:* ${url}
 
-${e} Espere un momento...`
-
-if (command == 'play') {
+${e} Espere un momento (Audio)...`
 let thumb = (await conn.getFile(thumbnail))?.data;
 
 let JT = {
@@ -50,6 +50,12 @@ console.error(error)
 }}
 
 if (command == 'play3') {
+let HS = `_*⚡YouTube-Play⚡*_\n\n*Duración:* ${timestamp}
+*Vistas*: ${vistas}
+*Uploaded:* ${ago}
+*Url:* ${url}
+
+${e} Espere un momento (audioDoc)...`
 let thumb = (await conn.getFile(thumbnail))?.data;
 
 let JT = {
@@ -67,8 +73,6 @@ try {
 let api = await fetch(`https://api.vreden.web.id/api/ytplaymp3?query=${url}`);
 let json = await api.json()
 let { download } = json.result
-
-//await conn.sendMessage(m.chat, { audio: { url: download.url }, caption: ``, mimetype: "audio/mpeg", }, { quoted: m })
 
 await conn.sendMessage(m.chat, { document: { url: download.url }, mimetype: 'audio/mpeg',fileName: `audio.mp3`, caption: `${m.pushName}` }, { quoted: m });
 

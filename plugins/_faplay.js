f1cf32d0b001fc173acd63151a95f8e64b42dd81
gudo1234 <izumilitee@gmail.com>
2025-01-19 19:33:44 -0600

    Update _faplay.js

--- plugins/_faplay.js
+++ plugins/_faplay.js
@@ -5,9 +5,8 @@ if (!args[0]) return m.reply("Ingresa el enlace del vídeo de YouTube")
 let name = await conn.getName(m.sender)
 await m.react('⌛')
 conn.sendMessage(m.chat, { text: global.espere + `*${m.pushName}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, thumbnailUrl: img.getRandom(), thumbnail: img.getRandom(), showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
-if (command == 'ytmp3') { // yta audio
+if (command == 'ytmp3' || command == 'yta') { // yta audio
     try {
-await
     await conn.sendMessage(m.chat, { audio: { url: `https://api.dorratz.com/v2/yt-mp3?url=${text}` }, mimetype: 'audio/mpeg'}, { quoted: m });
 }catch(e) {
         await m.react('❌')
@@ -15,9 +14,8 @@ await
         return console.log(e)
     }}
 
-if (command == 'ytmp4') { // mp4 video
+if (command == 'ytmp4' || command == 'ytv') { // mp4 video
     try {
-await
     await conn.sendMessage(m.chat, { video: { url: `https://api.dorratz.com/v2/yt-mp3?url=${text}` }, caption: `${name}` }, { quoted: m });
 }catch(e) {
         await m.react('❌')
@@ -25,9 +23,8 @@ await
         return console.log(e)
     }}
 
-if (command == 'ytmp3doc') { // ytadoc audio
+if (command == 'ytmp3doc' || command == 'ytadoc') { // ytadoc audio
     try {
-await
     await conn.sendMessage(m.chat, { document: { url: `https://api.dorratz.com/v2/yt-mp3?url=${text}` }, mimetype: 'audio/mpeg',fileName: `audio.mp3`,caption: `${name}` }, { quoted: m });
 }catch(e) {
         await m.react('❌')
@@ -35,9 +32,8 @@ await
         return console.log(e)
     }}
 
-if (command == 'ytmp4doc') { // mp4doc video
+if (command == 'ytmp4doc' || command == 'ytvdoc') { // mp4doc video
     try {
-await
     await conn.sendMessage(m.chat, { document: { url: `https://api.dorratz.com/v2/yt-mp3?url=${text}` }, mimetype: 'video/mp4',fileName: `video.mp4`,caption: `${name}` }, { quoted: m });
 }catch(e) {
         await m.react('❌')
@@ -46,5 +42,5 @@ await
     }}
 }
 
-handler.command = ['ytmp3', 'ytmp4', 'ytmp3doc', 'ytmp4doc'];
+handler.command = ['ytmp3', 'yta', 'ytmp4', 'ytv', 'ytmp3doc', 'ytadoc', 'ytmp4doc', 'ytvdoc'];
 export default handler;

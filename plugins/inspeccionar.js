2ef3b80eb8d84b2313e39527fed958eb9a7d1040
gudo1234 <izumilitee@gmail.com>
2025-01-20 16:33:54 -0600

    Update inspeccionar.js

--- plugins/inspeccionar.js
+++ plugins/inspeccionar.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-    if (!text) throw `${e} *Proporcione un link de un canal de WhatsApp junto al comando*`;
+    if (!text) throw `${e} *Proporcione un link de un canal de WhatsApp junto al comando*\n\n*Ejemplo:* ${usedPrefix + command} ${canal}`;
 
-    const regex = /https:\/\/chat\.whatsapp\.com\/([A-Za-z0-9]+)/;
+    const regex = /https:\/\/whatsapp\.com\/channel\/([A-Za-z0-9]+)/;
     const match = text.match(regex);
 
     if (match && match[1]) {

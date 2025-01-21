5b6a77ee01f48830a34a4a795efa177a70de6063
gudo1234 <izumilitee@gmail.com>
2025-01-21 15:25:50 -0600

    Update setppbot.js

--- plugins/setppbot.js
+++ plugins/setppbot.js
@@ -1,14 +1,13 @@
-import jimp from "jimp";
-import { S_WHATSAPP_NET } from '@whiskeysockets/baileys';
+import jimp from "jimp"
+import { S_WHATSAPP_NET } from '@whiskeysockets/baileys'
 
 let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
   try {
     let groupId = m.chat;
-    let quotedMsg = m.quoted ? m.quoted : m;
-    if (!m.quoted) return m.reply('Responde a una Imagen');
-
-    let media = await quotedMsg.download();
-    
+    let quotedMsg = m.quoted ? m.quoted : m
+    if (!m.quoted) return m.reply(`${e} *Responde a una Imagen.*`)
+    let mediaType = (quotedMsg.type || quotedMsg).mimetype || '';
+    var media = await quotedMsg.download();
     async function processImage(media) {
       const image = await jimp.read(media);
       const resizedImage = image.getWidth() > image.getHeight() ? image.resize(720, jimp.AUTO) : image.resize(jimp.AUTO, 720);
@@ -16,29 +15,26 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
         img: await resizedImage.getBufferAsync(jimp.MIME_JPEG),
       };
     }
-
     var { img: processedImage } = await processImage(media);
-    
-    await conn.query({
-      tag: 'iq',
-      attrs: {
-        to: S_WHATSAPP_NET,
-        type: 'set',
-        xmlns: 'w:profile:picture',
-      },
-      content: [
-        {
-          tag: 'picture',
-          attrs: { type: 'image' },
-          content: processedImage,
-        },
-      ],
-    });
-
-    m.reply('¡Icono actualizado exitosamente! 🎉');
+    conn.query({
+                tag: 'iq',
+                attrs: {
+                    to: cambiaporlafuncionQuedaelnumerodelbot ,
+                    type:'set',
+                    xmlns: 'w:profile:picture'
+                },
+                content: [
+                    {
+                        tag: 'picture',
+                        attrs: { type: 'image' },
+                        content: processedImage
+                    }
+                ]
+            })
+  m.reply(`${e} *Imagen actualizada.*`);
   } catch (error) {
-    console.error(error); // Para depuración
-    return m.react('❌');
+  return m.react('❌');
+
   }
 };
 

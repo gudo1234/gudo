273f80b07c36b1a436676e46d7c5a2dbad18ba71
gudo1234 <izumilitee@gmail.com>
2025-01-20 21:02:18 -0600

    Update que.js

--- plugins/que.js
+++ plugins/que.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix, command, participants, groupMetadata }) => {
 
-let img = await (await fetch(`https://qu.ax/casQP.jpg`)).buffer()
+//let img = await (await fetch(`https://qu.ax/casQP.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 conn.sendMessage(m.chat, { text: `🧀zo`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, thumbnailUrl: img.getRandom(), thumbnail: img.getRandom(), showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })

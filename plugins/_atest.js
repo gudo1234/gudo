dc5732770d39f050fe64d8cba2fefa121539deb0
gudo1234 <izumilitee@gmail.com>
2025-01-20 20:26:13 -0600

    Update and rename _foreaccion.js to _atest.js

---
+++ plugins/_atest.js
@@ -0,0 +1,29 @@
+const handler = async (m, {conn, args, usedPrefix, command}) => {
+
+const messages = [
+`hola`, 
+'', imagen,
+[],
+[],
+[[], ['🌎Canal', `${canal}`], ['🌱Intagram', `${ig}`]],
+[]
+], [ 
+`hola2`, 
+'', imagen2,
+[],
+[],
+[[], ['🗿Owner', `wa.me/50492280729`], ['👥Atom', `${nn}`]],
+[]
+], [ 
+`hola3`, 
+'', imagen3,
+[],
+[],
+[[], ['🤝Donar', `${edar}`], ['🔆Socializar', `${social}`]],
+[]
+]]
+conn.sendCarousel(m.chat, null, null, null, messages)
+}
+
+handler.command = ['test']
+export default handler

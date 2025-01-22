e0a9d8ab18fb5be2e7a135b5b5d4a38502700ebf
gudo1234 <izumilitee@gmail.com>
2025-01-21 21:09:41 -0600

    Update spotify.js

--- plugins/spotify.js
+++ plugins/spotify.js
@@ -1,103 +1,31 @@
-import axios from 'axios'
-import fetch from 'node-fetch'
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
-import search from 'yt-search'
+import { 
+  getOriginalUrl,
+  search,
+  downloads,
+  downloads2,
+  downloadAlbum,
+  downloadAlbum2,
+  downloadTrack,
+  downloadTrack2
+} from "@nechlophomeriaa/spotifydl"
 
-async function spotifyxv(query) {
-  let token = await tokens();
-  let response = await axios({
-    method: 'get',
-    url: 'https://api.spotify.com/v1/search?q=' + encodeURIComponent(query) + '&type=track',
-    headers: {
-      Authorization: 'Bearer ' + token,
-    },
-  })
-  const tracks = response.data.tracks.items
-  const results = tracks.map((track) => ({
-    name: track.name,
-    artista: track.artists.map((artist) => artist.name),
-    album: track.album.name,
-    duracion: timestamp(track.duration_ms),
-    url: track.external_urls.spotify,
-    imagen: track.album.images.length ? track.album.images[0].url : '',
-  }))
-  return results
-}
+let handler = async (m, { conn,text, args, usedPrefix, command }) => {
+    if (!text) throw `${e} Ingresa una busqueda de ${command}`
+            let downTrack = await downloadTrack2(`${text}`)
+            let caption2=`★━━━━━━━━━━━━━━━━━━━━★
+        🎶 𝐒𝐩𝐨𝐭𝐢𝐟𝐲 𝐓𝐫𝐚𝐜𝐤 𝐃𝐨𝐰𝐥𝐨𝐚𝐝𝐞𝐫 🎶\n
+        𝘼𝙧𝙩𝙞𝙨𝙩𝙖:${downTrack.artists}\n
+        𝐓í𝐭𝐮𝐥𝐨:${downTrack.title}\n
+        𝐃𝐮𝐫𝐚𝐜𝐢ó𝐧:${downTrack.duration}\n
+★━━━━━━━━━━━━━━━━━━━━★`
+            await conn.sendMessage(m.chat, {image: {url: downTrack.imageUrl}, caption: `${caption2}`}, {quoted: m});
+            await conn.sendMessage(m.chat, {audio: downTrack.audioBuffer, fileName: `${downTrack.title}.mp3`, mimetype: 'audio/mpeg'}, {quoted: m});
+            break;
 
-async function tokens() {
-  const response = await axios({
-    method: 'post',
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Authorization: 'Basic ' + Buffer.from('acc6302297e040aeb6e4ac1fbdfd62c3:0e8439a1280a43aba9a5bc0a16f3f009').toString('base64'),
-    },
-    data: 'grant_type=client_credentials',
-  })
-  return response.data.access_token
-}
+            m.reply("No soportado")      
+    }
+   
 
-function timestamp(time) {
-  const minutes = Math.floor(time / 60000);
-  const seconds = Math.floor((time % 60000) / 1000);
-  return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-}
 
-async function getBuffer(url, options) {
-  try {
-    options = options || {};
-    const res = await axios({
-      method: 'get',
-      url,
-      headers: {
-        DNT: 1,
-        'Upgrade-Insecure-Request': 1,
-      },
-      ...options,
-      responseType: 'arraybuffer',
-    });
-    return res.data;
-  } catch (err) {
-    return err;
-  }
-}
-
-async function getTinyURL(text) {
-try {
-    let response = await axios.get(`https://tinyurl.com/api-create.php?url=${text}`);
-    return response.data;
-  } catch (error) {
-    return text;
-  }
-}
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `${e} *Uso Correcto*\n\nEjemplo:\n${usedPrefix + command} save yours tear`
-try {
-await m.react('⬆️')
-let songInfo = await spotifyxv(text)
-if (!songInfo.length) throw `*No se encontró una canción.*`
-let res = songInfo[0]
-let fileSizeInMB = (await getBuffer(res.url)).length / (1024 * 1024)
-let shortURL = await getTinyURL(res.url)
-let name = await conn.getName(m.sender)
-await conn.sendMessage(m.chat, { text: global.espere + `*${name}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, humbnailUrl: imagen4, thumbnail: imagen4, showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
-let info = `\t\t\tli.*📀SPOTịFY📀*.il\n\t\t\t*Nombre:* ${res.name}\n\t\t\t*Artista:* ${res.artista.join(', ')}\n\t\t\t*Album:* ${res.album}\n\t\t\t*Duracion:* ${res.duracion}\n\t\t\t*Enlace:* ${shortURL}`
-let resImg = await fetch(res.imagen)
-let thumbb = await resImg.buffer()
-let { videos } = await search(res.name)
-let q = '128kbps'
-let v = videos[0].url
-let yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v))
-let dl_url = await yt.audio[q].download()
-let ttl = await yt.title
-let size = await yt.audio[q].fileSizeH
-conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: `${ttl}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m })
-let img = await getBuffer(res.imagen)
-await conn.sendMessage(m.chat, { text: info, contextInfo: { mentionedJid: [m.sender], forwardingScore: 90, externalAdReply: { title: '꙳🧧𓆩ίʑ᭘ɱί-ⲃⲟτ𓆪🧧꙳', body: 'spotify', thumbnail: img, thumbnailUrl: img, sourceUrl: 'Loadding...', mediaType: 1 }}}, { quoted: m })
-await m.react('⬇️')  
-} catch (error) {
-}}
-handler.command = ['spotify', 'music']
-handler.group = true;
+handler.command = ['spotify', 'spotifypro', 'spotifydl']
 export default handler

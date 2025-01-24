57b5634b9f0013616e392a2b26aa781bd6570200
gudo1234 <izumilitee@gmail.com>
2025-01-23 18:48:22 -0600

    Update config.js

--- config.js
+++ config.js
@@ -71,7 +71,7 @@ global.moment = moment
 global.packname = '꙳🧧𓆩ίʑ᭘ɱί-ⲃⲟτ𓆪🧧꙳'
 global.author = 'ᯓ᮫݃͜ᮨ🍁ܾ݉ᢥ౽꯭ⲉυ꯭᥉′🦦ꪃꒉܻᵃₚͬ៰⍳'
 global.espere = 'ᯓ〽️ _Espere un momento..._'
-global.e = '📍'
+global.e = '🌙᪶⃛▹'
 //------------[ Versión | Nombre | cuentas ]------------
 
 global.wm = '꙳🧧𓆩ίʑ᭘ɱί-ⲃⲟτ𓆪🧧꙳'

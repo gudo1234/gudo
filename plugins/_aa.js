09bfd1dec0847d6b0df44836ffb8aabadc8d0b8a
gudo1234 <izumilitee@gmail.com>
2025-02-04 22:54:52 -0600

    Create _aa.js

---
+++ plugins/_aa.js
@@ -0,0 +1,60 @@
+import PhoneNumber from 'awesome-phonenumber'
+
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => 'https://qu.ax/csvrl.jpg')
+let biografia = await conn.fetchStatus('50492280729' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let biografiaBot = await conn.fetchStatus(`${conn.user.jid.split('@')[0]}` +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let bio = biografia.status?.toString() || 'Sin Biografía'
+let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
+let name = await conn.getName(who)
+
+  await sendContactArray(conn, m.chat, [
+     ['hola', `👑 Propietario`, `${author}`, canal, nn, `Honduras`, 'xd', bio],
+['50488723207', `Es Un Bot`, `${packname}`, `📵 No Hacer Spam`, correo, 'Honduras', wm, 'jaja']
+], m)
+  }
+
+handler.help = ["creador","owner"]
+handler.tags = ["info"]
+handler.command = ['.put']
+export default handler
+
+async function sendContactArray(conn, jid, data, quoted, options) {
+        if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
+                let contacts = []
+        for (let [number, name, isi, isi1, isi2, isi3, isi4, isi5] of data) {
+            number = number.replace(/[^0-9]/g, '')
+            let njid = number + '@s.whatsapp.net'
+            let biz = await conn.getBusinessProfile(njid).catch(_ => null) || {}
+            // N:;${name.replace(/\n/g, '\\n').split(' ').reverse().join(';')};;;
+            let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:Sy;Bot;;;
+FN:${name.replace(/\n/g, '\\n')}
+item.ORG:${isi}
+item1.TEL;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}
+item1.X-ABLabel:${isi1}
+item2.EMAIL;type=INTERNET:${isi2}
+item2.X-ABLabel:📧 Email
+item3.ADR:;;${isi3};;;;
+item3.X-ABADR:ac
+item3.X-ABLabel:🏷 Region
+item4.URL:${isi4}
+item4.X-ABLabel:Website
+item5.X-ABLabel:${isi5}
+END:VCARD`.trim()
+            contacts.push({ vcard, displayName: name })
+        }
+        return await conn.sendMessage(jid, {
+            contacts: {
+                displayName: (contacts.length > 1 ? `2013 kontak` : contacts[0].displayName) || null,
+                contacts,
+            }
+        },
+        {
+            quoted,
+            ...options
+        })
+}

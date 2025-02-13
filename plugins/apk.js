bf374c6680906374c2356d9ae932754de19e7ac2
gudo1234 <izumilitee@gmail.com>
2025-02-12 19:10:48 -0600

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -18,7 +18,7 @@ if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
 return await conn.reply(m.chat, `${e} *El archivo es demaciado pesado.*`, m)}
 await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m})
 } catch {
-conn.sendButton(m.chat, `${e} Ocurrió un error temporal, toque el botón reintentar...`, wm, m, [['Reintentar', `.apk2 ${text}`]], null, null, m)
+conn.sendButton(m.chat, `${e} Ocurrió un error temporal, toque el botón reintentar...`, wm, null, [['Reintentar', `.apk2 ${text}`]], null, null, m)
   
 //return conn.reply(m.chat, '✖️ *Ocurrió un fallo*', m, rcanal )}}
 

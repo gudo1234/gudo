ec03d7d39ab88f62da4de651abb52e9145a40022
gudo1234 <izumilitee@gmail.com>
2025-02-13 20:18:16 -0600

    Create _boton.js

---
+++ plugins/_boton.js
@@ -0,0 +1,72 @@
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+
+conn.sendMessage(m.chat, {
+ image: { url: 'https://files.catbox.moe/iejoer.jpg'},
+ caption: `${e} que pasa hermosa`,
+footer: wm,
+ contextInfo: {
+mentionedJid: [m.sender],
+forwardingScore: 999,
+isForwarded: true,
+externalAdReply: {
+  showAdAttribution: true, 
+  title: `Holaa`,
+  body: "like your pussycat",
+  thumbnailUrl: 'https://cdn.arifzyn.site/f/sy6tjbzk.jpg',
+  sourceUrl: canal,
+  mediaType: 1,
+  renderLargerThumbnail: false
+}}, 
+  buttons: [
+  {
+ buttonId: '.ping',
+ buttonText: {
+displayText: 'ping'
+ },
+ type: 1,
+  },
+  {
+ buttonId: '.owner',
+ buttonText: {
+displayText: 'owner'
+ },
+ type: 1,
+  },
+  {
+ type: 4,
+ nativeFlowInfo: {
+name: 'single_select',
+paramsJson: JSON.stringify({
+  title: 'Dont click',
+  sections: [
+ {
+title: 'my focking bicht',
+highlight_label: '',
+rows: [
+  {
+ header: '⌬ Message',
+ title: 'menu',
+ description: `i like pussydog`, 
+ id: '.menu',
+  },
+  {
+ header: '⌬ Message',
+ title: 'check ping',
+ description: `i like pussycat`, 
+ id: '.ping',
+  },
+],
+ },
+  ],
+}),
+ },
+  },
+  ],
+  headerType: 1,
+  viewOnce: true
+})
+
+}
+handler.command = ['boton']
+handler.group = true
+export default handler

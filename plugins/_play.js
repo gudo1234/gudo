a3474750663d91f17ec49067bb13bf987e0b984d
gudo1234 <izumilitee@gmail.com>
2025-02-15 10:25:59 -0600

    Update _play.js

--- plugins/_play.js
+++ plugins/_play.js
@@ -38,15 +38,20 @@ let UC = RM.key.remoteJid
 
 if (RM.message.extendedTextMessage?.contextInfo?.stanzaId === SM.key.id) {
 if (UR === '1' || UR === 'Audio') {
+  m.react('🕒')
   await conn.sendMessage(UC, { audio: { url: dataAud.result.downloadUrl }, mimetype: "audio/mpeg", caption: null }, { quoted: RM })
 } else if (UR === '2' || UR === 'Video') {
+  m.react('🕒')
   await conn.sendMessage(m.chat, { video: { url: dataVid.result.downloadUrl }, caption: ``, mimetype: 'video/mp4', fileName: `${res[0].title}` + `.mp4`}, {quoted: m })
 } else if (UR === '3' || UR === 'Doc audio') {
-await conn.sendMessage(m.chat, { document: { url: dataAud.result.downloadUrl }, fileName: `${res[0].title}`, mimetype: "audio/mpeg" }, { quoted: m });
+  m.react('🕒') 
+  await conn.sendMessage(m.chat, { document: { url: dataAud.result.downloadUrl }, fileName: `${res[0].title}`, mimetype: "audio/mpeg" }, { quoted: m });
 } else if (UR === '4' || UR === 'Doc video') {
-await conn.sendMessage(m.chat, { document: { url: dataVid.result.downloadUrl }, fileName: `${res[0].title}`, mimetype: 'video/mp4', }, { quoted: m });
+  m.react('🕒')
+  await conn.sendMessage(m.chat, { document: { url: dataVid.result.downloadUrl }, fileName: `${res[0].title}`, mimetype: 'video/mp4', }, { quoted: m });
 } else {
-await conn.sendMessage(UC, { text: "Opcion invalida, responde con 1 *(audio)* o 2 *(video)*." }, { quoted: RM })
+  m.react('🕒')
+  await conn.sendMessage(UC, { text: "Opcion invalida, responde con 1 *(audio)* o 2 *(video)*." }, { quoted: RM })
 }}})
 
 } catch (error) {

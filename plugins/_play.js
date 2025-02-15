e2afede8d0d1cdb2baa899b8e95b2d5a5662423f
gudo1234 <izumilitee@gmail.com>
2025-02-15 07:46:49 -0600

    Update _play.js

--- plugins/_play.js
+++ plugins/_play.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+/*import fetch from 'node-fetch'
 import yts from 'yt-search'
 
 let handler = async (m, { conn, text, args }) => {
@@ -47,7 +47,74 @@ await conn.sendMessage(UC, { text: "Opcion invalida, responde con 1 *(audio)* o 
 console.error(error)
 }}
 
-handler.command = ['play']
+handler.command = ['play', 'play2']
+
+export default handler
+
+async function search(query, options = {}) {
+  let search = await yts.search({ query, hl: "es", gl: "ES", ...options })
+  return search.videos
+}*/
+
+
+// *𓍯𓂃𓏧♡  PLAY (audio - video)*
+
+import fetch from 'node-fetch'
+import yts from 'yt-search'
+
+let handler = async (m, { conn, text, args }) => {
+if (!text)  return conn.reply(m.chat, `❀ Ingresa el nombre de lo que quieres buscar`, m)
+
+
+try {
+let res = await search(args.join(" "))
+
+let apiAud = await fetch(`https://api.agungny.my.id/api/youtube-audio?url=${'https://youtu.be/' + res[0].videoId}`)
+let dataAud = await apiAud.json()
+let apiVid = await fetch(`https://api.agungny.my.id/api/youtube-video?url=${'https://youtu.be/' + res[0].videoId}`)
+let dataVid = await apiVid.json()
+
+
+let txt = `*◆ [ YOUTUBE - PLAY ] ◆*
+- *Titulo:* ${res[0].title}
+- *Duracion:* ${res[0].timestamp}
+- *Visitas:* ${res[0].views}
+- *Subido:* ${res[0].ago}
+
+◆────────────────◆
+
+Responde a este mensaje dependiendo lo que quieras :
+
+1 : Audio
+2 : Video`
+
+let SM = await conn.sendFile(m.chat, res[0].thumbnail, 'HasumiBotFreeCodes.jpg', txt, m)
+conn.ev.on("messages.upsert", async (upsertedMessage) => {
+let RM = upsertedMessage.messages[0];
+if (!RM.message) return
+
+const UR = RM.message.conversation || RM.message.extendedTextMessage?.text
+let UC = RM.key.remoteJid
+
+if (RM.message.extendedTextMessage?.contextInfo?.stanzaId === SM.key.id) {
+
+if (UR === '1' || UR === 'Audio') {
+  await conn.sendMessage(UC, { audio: { url: dataAud.result.downloadUrl }, mimetype: "audio/mpeg", caption: null }, { quoted: RM })
+} else if (UR === '2' || UR === 'Video') {
+  await conn.sendMessage(m.chat, { video: { url: dataVid.result.downloadUrl }, caption: ``, mimetype: 'video/mp4', fileName: `${res[0].title}` + `.mp4`}, {quoted: m })
+} else if (UR === '3' || UR === 'Doc audio') {
+await conn.sendMessage(m.chat, { document: { url: dataAud.result.downloadUrl }, fileName: `${res[0].title}`, mimetype: "audio/mpeg" }, { quoted: m });
+} else if (UR === '4' || UR === 'Doc video') {
+await conn.sendMessage(m.chat, { document: { url: dataVid.result.downloadUrl }, fileName: `${res[0].title}`, mimetype: 'video/mp4', caption: `${e} Aqui tienes ฅ^•ﻌ•^ฅ.`, thumbnail: thumb }, { quoted: m });
+} else {
+await conn.sendMessage(UC, { text: "Opcion invalida, responde con 1 *(audio)* o 2 *(video)*." }, { quoted: RM })
+}}})
+
+} catch (error) {
+console.error(error)
+}}
+
+handler.command = ['play', 'play2']
 
 export default handler
 

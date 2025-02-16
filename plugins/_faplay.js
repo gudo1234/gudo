56b2f0dd9e5d8acfc9100629e928c1c3a55fbfb2
gudo1234 <izumilitee@gmail.com>
2025-02-15 22:39:25 -0600

    Update _faplay.js

--- plugins/_faplay.js
+++ plugins/_faplay.js
@@ -1,29 +1,29 @@
-/*import axios from "axios"
+import axios from "axios"
 
 let handler = async (m, { conn, text, usedPrefix, command, args }) => {
 if (!args[0]) return m.reply(`${e} *Ingresa el enlace del vídeo de YouTube*`)
 let name = await conn.getName(m.sender)
 await m.react('⌛')
 conn.sendMessage(m.chat, { text: global.espere + `*${m.pushName}*`, contextInfo: { externalAdReply: {title: `${wm}`, body: `${await conn.getName(m.chat)}`, thumbnailUrl: img.getRandom(), thumbnail: img.getRandom(), showAdAttribution: true, sourceUrl: canal}}} , { quoted: fkontak })
-if (command == 'ytmp3' || command == 'yta') { // yta audio
+if (command == 'pla' || command == 'yta') { // yta audio
     try {
-    await conn.sendMessage(m.chat, { audio: { url: `https://api.dorratz.com/v2/yt-mp3?url=${text}` }, mimetype: 'audio/mpeg'}, { quoted: m });
+    await conn.sendMessage(m.chat, { audio: { url: `https://api.agungny.my.id/api/youtube-audio?url=${'https://youtu.be/' + res[0].videoId}` }, mimetype: 'audio/mpeg'}, { quoted: m });
 }catch(e) {
         await m.react('❌')
         m.reply(`Error, ${e}`)
         return console.log(e)
     }}
 
-if (command == 'ytmp4' || command == 'ytv') { // mp4 video
+if (command == 'pla2' || command == 'x') { // mp4 video
     try {
-    await conn.sendMessage(m.chat, { video: { url: `https://api.dorratz.com/v2/yt-mp3?url=${text}` }, caption: `${name}` }, { quoted: m });
+    await conn.sendMessage(m.chat, { video: { url: `https://api.agungny.my.id/api/youtube-audio?url=${'https://youtu.be/' + res[0].videoId}` }, caption: `${name}` }, { quoted: m });
 }catch(e) {
         await m.react('❌')
         m.reply(`Error, ${e}`)
         return console.log(e)
     }}
 
-if (command == 'ytmp3doc' || command == 'ytadoc') { // ytadoc audio
+if (command == 'pla3' || command == 'x') { // ytadoc audio
     try {
     await conn.sendMessage(m.chat, { document: { url: `https://api.dorratz.com/v2/yt-mp3?url=${text}` }, mimetype: 'audio/mpeg',fileName: `audio.mp3`,caption: `${name}` }, { quoted: m });
 }catch(e) {
@@ -32,7 +32,7 @@ if (command == 'ytmp3doc' || command == 'ytadoc') { // ytadoc audio
         return console.log(e)
     }}
 
-if (command == 'ytmp4doc' || command == 'ytvdoc') { // mp4doc video
+if (command == 'pla4' || command == 'x') { // mp4doc video
     try {
     await conn.sendMessage(m.chat, { document: { url: `https://api.dorratz.com/v2/yt-mp3?url=${text}` }, mimetype: 'video/mp4',fileName: `video.mp4`,caption: `${name}` }, { quoted: m });
 }catch(e) {
@@ -42,5 +42,5 @@ if (command == 'ytmp4doc' || command == 'ytvdoc') { // mp4doc video
     }}
 }
 
-handler.command = ['ytmp3', 'yta', 'ytmp4', 'ytv', 'ytmp3doc', 'ytadoc', 'ytmp4doc', 'ytvdoc'];
-export default handler;*/
+handler.command = ['pla', 'pla2', 'pla3', 'pla4'];
+export default handler;

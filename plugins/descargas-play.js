7f3c51e57eea8247aae262b192160f501dc5bc35
gudo1234 <izumilitee@gmail.com>
2025-02-23 20:47:16 -0600

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -10,37 +10,20 @@ let HS = async (m, { conn, text }) => {
     let api = await (await fetch(`https://api.lyrax.net/api/dl/ytdl?url=${text}&apikey=0a2cc90e`)).json()
     let { file_url, title } = api.data
     let { type, size, duration } = api.data.info
-//Para audio 
-if (command === 'play' || command === 'yta' || command === 'ytmp3') {
-await conn.sendMessage(m.chat, { audio: { url: file_url }, mimetype: 'audio/mpeg', fileName: json.data.filename }, { quoted: m })};
-m.react('✅')
-  
-//Para video
-if (command === 'play2' || command === 'ytv' || command === 'ytmp4' || command === 'playvid') {
+
     await conn.sendMessage(m.chat, {
       video: { url: file_url },
       fileName: `${title}.mp4`,
       mimetype: 'video/mp4',
       caption: `🌿 Titulo: ${title}\n🌲 Duración: ${duration}\n🌴 Peso: ${size}\n🌾 Tipo: ${type}`
-    }, { quoted: m })}
-    m.react('✅')
-
-//Para DocVideo
-if (command === 'play4' || command === 'ytvdoc' || command === 'ytmp4doc') {
-await conn.sendMessage(m.chat, { document: { url: file_url }, mimetype: 'video/mp4' ,fileName: `${title}`,caption: `${m.pushName}` }, { quoted: m })};
-    m.react('✅')
-
-//Para DocAudio
-if (command === 'play3' || command === 'ytadoc' || command === 'ytmp3doc') {
-await conn.sendMessage(m.chat, { document: { url: file_url }, mimetype: 'audio/mpeg',fileName: `${title}`,caption: `${m.pushName}` }, { quoted: m })};
-    m.react('✅')
-} catch (error) {
+    }, { quoted: m })
+  } catch (error) {
     console.error(error)
-    conn.reply(m.chat, "❀ Hubo un error al obtener el DocAudio, intenta nuevamente.", m)
+    conn.reply(m.chat, "❀ Hubo un error al obtener el video, intenta nuevamente.", m)
   }
 }
 
-HS.command = ['play', 'yta', 'ytmp3', 'play2', 'playvid', 'ytv', 'ytmp4', 'play4', 'ytvdoc', 'ytmp4doc', 'play3', 'ytadoc', 'ytmp3doc']
+HS.command = ['ytmp4', 'ytv']
 export default HS
 
 function isYouTubeUrl(text) {
